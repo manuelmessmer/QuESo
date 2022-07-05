726e008690527c2a1a84c89f810e450b55027f94
ge69vud <manuel.messmer@tum.de>
2022-07-05 16:52:23 +0200

    Minor.

--- configure.sh
+++ configure.sh
@@ -22,7 +22,6 @@ export PYTHON_EXECUTABLE=${PYTHON_EXECUTABLE:-"/usr/bin/python3"}
 
 # Configure
 cmake -H"${APP_SOURCE}" -B"${APP_BUILD}/${CMAKE_BUILD_TYPE}" \
--DCGAL_DIR="/home/manuel/Programs/CGAL/CGAL-5.4" \
 
 # Buid
 cmake --build "${APP_BUILD}/${CMAKE_BUILD_TYPE}" --target install -- -j8
