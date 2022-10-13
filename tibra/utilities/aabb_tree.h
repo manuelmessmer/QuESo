9218bbb38d3795e4697f6a8f8db2b1c3a318387b
manuel.messmer <manuel.messmer@tum.de>
2022-10-13 11:49:40 -0600

    update AABB tree.

--- tibra/utilities/aabb_tree.h
+++ tibra/utilities/aabb_tree.h
@@ -16,7 +16,9 @@
 
 // Create alias for AABB
 typedef aabb_base::AABB_base AABB;
-constexpr double kEpsilon = 1e-8;
+
+constexpr double kEpsilon = 1e-14;
+
 ///@name TIBRA Classes
 ///@{
 
@@ -24,7 +26,8 @@ constexpr double kEpsilon = 1e-8;
 /**
  * @class  Ray
  * @author Manuel Messmer
- * @brief  Ray to be used in AABB_tree
+ * @brief  Ray to be used in AABB_tree. Direction of ray must be positive oriented.
+ *         Check is omitted for better performance.
 */
 class Ray {
 public:
@@ -42,25 +45,18 @@ public:
     Ray(const Vector3d& Origin, const Vector3d& Direction) :
         mOrigin(Origin), mDirection(Direction)
     {
-        mInvDirection[0] = 1 / Direction[0];
-        mInvDirection[1] = 1 / Direction[1];
-        mInvDirection[2] = 1 / Direction[2];
-
-        mSign[0] = (mInvDirection[0] < 0);
-        mSign[1] = (mInvDirection[1] < 0);
-        mSign[2] = (mInvDirection[2] < 0);
+        mInvDirection[0] = 1.0 / Direction[0];
+        mInvDirection[1] = 1.0 / Direction[1];
+        mInvDirection[2] = 1.0 / Direction[2];
     }
 
     ///@}
     ///@name Operations
     ///@{
-    /// The code from this lesson returns intersections with the box that are either in front or
-    /// behind the origin of the ray. For instance, if the ray's origin is inside the box (adjacent image),
-    /// there will be two intersections: one in front of the ray and one behind. We know that an intersection
-    /// is "behind" the origin of the ray when t is negative. When t is positive, the intersection is in front
-    /// of the origin of the ray (with respect to the ray's direction).
 
-    //bool intersect(const Ray &r, double &t) const
+    ///@brief Returns true if origin of ray is inside aabb.
+    ///@param aabb
+    ///@return bool
     bool inside(AABB &aabb)
     {
         if(    mOrigin[0] < aabb.lowerBound[0]
@@ -75,40 +71,67 @@ public:
         return true;
     }
 
+    ///@brief Returns true if ray intersects aabb.
+    ///@param aabb
+    ///@param t distance to intersection.
+    ///@return bool
     bool intersect(AABB &aabb, double &t)
     {
         double tmin, tmax, tymin, tymax, tzmin, tzmax;
-        //std::cout << "test " << std::endl;
-        std::array<Vector3d,2> bounds{};
-        std::copy_n(aabb.lowerBound.begin(), 3, bounds[0].begin());
-        std::copy_n(aabb.upperBound.begin(), 3, bounds[1].begin());
 
-        tmin = (bounds[mSign[0]][0] - mOrigin[0]) * mInvDirection[0];
-        tmax = (bounds[1-mSign[0]][0] - mOrigin[0]) * mInvDirection[0];
-        tymin = (bounds[mSign[1]][1] - mOrigin[1]) * mInvDirection[1];
-        tymax = (bounds[1-mSign[1]][1] - mOrigin[1]) * mInvDirection[1];
+        double lower_0 = aabb.lowerBound[0];
+        double lower_1 = aabb.lowerBound[1];
 
-        if ((tmin > tymax) || (tymin > tmax))
+        double upper_0 = aabb.upperBound[0];
+        double upper_1 = aabb.upperBound[1];
+
+        double origin_0 = mOrigin[0];
+        double origin_1 = mOrigin[1];
+
+        double inv_direction_0 = mInvDirection[0];
+        double inv_direction_1 = mInvDirection[1];
+
+
+        tmin = (lower_0 - origin_0) * inv_direction_0;
+        tymax = (upper_1 - origin_1) * inv_direction_1;
+        if(tmin > tymax) {
+            return false;
+        }
+
+        tmax = (upper_0 - origin_0) * inv_direction_0;
+        tymin = (lower_1 - origin_1) * inv_direction_1;
+
+
+        if( tymin > tmax )
             return false;
 
         if (tymin > tmin)
             tmin = tymin;
+
         if (tymax < tmax)
             tmax = tymax;
 
-        tzmin = (bounds[mSign[2]][2] -  mOrigin[2]) * mInvDirection[2];
-        tzmax = (bounds[1-mSign[2]][2] -  mOrigin[2]) * mInvDirection[2];
+        double lower_2 = aabb.lowerBound[2];
+        double upper_2 = aabb.upperBound[2];
+        double origin_2 = mOrigin[2];
+        double inv_direction_2 = mInvDirection[2];
 
-        if ((tmin > tzmax) || (tzmin > tmax))
+
+        tzmin = (lower_2 - origin_2) * inv_direction_2;
+        if((tzmin > tmax))
+            return false;
+
+        tzmax = (upper_2 - origin_2) * inv_direction_2;
+        if( tmin > tzmax )
             return false;
 
         if (tzmin > tmin)
             tmin = tzmin;
-        if (tzmax < tmax)
-            tmax = tzmax;
 
         t = tmin;
         if (t < 0) {
+            if (tzmax < tmax)
+                tmax = tzmax;
             t = tmax;
             return false;
         }
@@ -116,124 +139,65 @@ public:
         return true;
     }
 
+    ///@brief Returns true if ray intersects triangle (only checks intersections in positive direction).
+    ///@param v0 Triangle Vertex 1
+    ///@param v1 Triangle Vertex 2
+    ///@param v2 Triangle Vertex 3
+    ///@param t Distance to intersection.
+    ///@param u Parametric coordinate 1.
+    ///@param v Parametric coordinate 2.
+    ///@return bool
+    bool intersect(
+        const Vector3d &v0, const Vector3d &v1, const Vector3d &v2,
+        double &t, double &u, double &v)
+    {
+        // Substraction: v1-v0 and v2-v0
+        Vector3d v0v1 = {v1[0] - v0[0], v1[1] - v0[1], v1[2] - v0[2]};
+        Vector3d v0v2 = {v2[0] - v0[0], v2[1] - v0[1], v2[2] - v0[2]};
 
+        // Cross product: mDirection x v0v2
+        Vector3d pvec = {mDirection[1]*v0v2[2] - mDirection[2]*v0v2[1],
+                        mDirection[2]*v0v2[0] - mDirection[0]*v0v2[2],
+                        mDirection[0]*v0v2[1] - mDirection[1]*v0v2[0]};
 
-inline
-double deg2rad(const double &deg)
-{ return deg * M_PI / 180; }
+        // Dot product: v0v1 * pvec
+        double det = v0v1[0]*pvec[0] + v0v1[1]*pvec[1] + v0v1[2]*pvec[2];
 
-inline
-double clamp(const double &lo, const double &hi, const double &v)
-{ return std::max(lo, std::min(hi, v)); }
+        if (fabs(det) < kEpsilon)
+            return false;
 
-bool rayTriangleIntersect(
-    const Vector3d &v0, const Vector3d &v1, const Vector3d &v2,
-    double &t, double &u, double &v)
-{
-    //#ifdef MOLLER_TRUMBORE
-    Vector3d v0v1 = {v1[0] - v0[0], v1[1] - v0[1], v1[2] - v0[2]};
-    Vector3d v0v2 = {v2[0] - v0[0], v2[1] - v0[1], v2[2] - v0[2]};
+        double invDet = 1 / det;
 
-    // Cross product
-    Vector3d pvec = {mDirection[1]*v0v2[2] - mDirection[2]*v0v2[1],
-                     mDirection[2]*v0v2[0] - mDirection[0]*v0v2[2],
-                     mDirection[0]*v0v2[1] - mDirection[1]*v0v2[0]};
+        // Substraction: mOrigin - v0
+        Vector3d tvec = {mOrigin[0] - v0[0], mOrigin[1] - v0[1], mOrigin[2] - v0[2]};
 
-    // Dot product
-    double det = v0v1[0]*pvec[0] + v0v1[1]*pvec[1] + v0v1[2]*pvec[2];
-    //#ifdef CULLING
-        // if the determinant is negative the triangle is backfacing
-        // if the determinant is close to 0, the ray misses the triangle
-    if (fabs(det) < kEpsilon)
-        return false;
-    // #else
-    //     // ray and triangle are parallel if det is close to 0
-    //     if (fabs(det) < kEpsilon) return false;
-    //#endif
-    double invDet = 1 / det;
+        // Dot product x invDet: (tvec * pvec) * invDet
+        u = (tvec[0]*pvec[0] + tvec[1]*pvec[1] + tvec[2]*pvec[2]) * invDet;
 
-    // Subtraction
-    Vector3d tvec = {mOrigin[0] - v0[0], mOrigin[1] - v0[1], mOrigin[2] - v0[2]};
+        if (u < 0 || u > 1)
+            return false;
 
-    // Dot product x invDet
-    u = (tvec[0]*pvec[0] + tvec[1]*pvec[1] + tvec[2]*pvec[2]) * invDet;
-    //u = tvec.dotProduct(pvec) * invDet;
+        // Cross product: tvec x v0v1
+        Vector3d qvec = {tvec[1]*v0v1[2] - tvec[2]*v0v1[1],
+                        tvec[2]*v0v1[0] - tvec[0]*v0v1[2],
+                        tvec[0]*v0v1[1] - tvec[1]*v0v1[0]};
 
-    if (u < 0 || u > 1)
-        return false;
+        // Dot product x invDet: (mDirection * qvec) * invDet
+        v = (mDirection[0]*qvec[0] + mDirection[1]*qvec[1] + mDirection[2]*qvec[2]) * invDet;
 
-    // Cross product
-    Vector3d qvec = {tvec[1]*v0v1[2] - tvec[2]*v0v1[1],
-                     tvec[2]*v0v1[0] - tvec[0]*v0v1[2],
-                     tvec[0]*v0v1[1] - tvec[1]*v0v1[0]};
+        if (v < 0 || u + v > 1)
+            return false;
 
-    // Dot product x invDet
-    v = (mDirection[0]*qvec[0] + mDirection[1]*qvec[1] + mDirection[2]*qvec[2]) * invDet;
+        // Dot product: v0v2 * qvec
+        t = (v0v2[0]*qvec[0] + v0v2[1]*qvec[1] + v0v2[2]*qvec[2]) * invDet;
 
-    if (v < 0 || u + v > 1)
-        return false;
+        // Return true if ray intersects in negative direction.
+        if( t < 0 )
+            return false;
 
-    t = (v0v2[0]*qvec[0] + v0v2[1]*qvec[1] + v0v2[2]*qvec[2]) * invDet;
-
-    if( t < 0 )
-        return false;
-
-    return true;
-    // #else
-    //     // compute plane's normal
-    //     Vec3f v0v1 = v1 - v0;
-    //     Vec3f v0v2 = v2 - v0;
-    //     // no need to normalize
-    //     Vec3f N = v0v1.crossProduct(v0v2);  //N
-    //     double denom = N.dotProduct(N);
-
-    //     // Step 1: finding P
-
-    //     // check if ray and plane are parallel ?
-    //     double NdotRayDirection = N.dotProduct(dir);
-
-    //     if (fabs(NdotRayDirection) < kEpsilon)  //almost 0
-    //         return false;  //they are parallel so they don't intersect !
-
-    //     // compute d parameter using equation 2
-    //     double d = -N.dotProduct(v0);
-
-    //     // compute t (equation 3)
-    //     t = -(N.dotProduct(orig) + d) / NdotRayDirection;
-
-    //     // check if the triangle is in behind the ray
-    //     if (t < 0) return false;  //the triangle is behind
-
-    //     // compute the intersection point using equation 1
-    //     Vec3f P = orig + t * dir;
-
-    //     // Step 2: inside-outside test
-    //     Vec3f C;  //vector perpendicular to triangle's plane
-
-    //     // edge 0
-    //     Vec3f edge0 = v1 - v0;
-    //     Vec3f vp0 = P - v0;
-    //     C = edge0.crossProduct(vp0);
-    //     if (N.dotProduct(C) < 0) return false;  //P is on the right side
-
-    //     // edge 1
-    //     Vec3f edge1 = v2 - v1;
-    //     Vec3f vp1 = P - v1;
-    //     C = edge1.crossProduct(vp1);
-    //     if ((u = N.dotProduct(C)) < 0)  return false;  //P is on the right side
-
-    //     // edge 2
-    //     Vec3f edge2 = v0 - v2;
-    //     Vec3f vp2 = P - v2;
-    //     C = edge2.crossProduct(vp2);
-    //     if ((v = N.dotProduct(C)) < 0) return false;  //P is on the right side;
-
-    //     u /= denom;
-    //     v /= denom;
-
-    //     return true;  //this ray hits the triangle
-    // #endif
+        return true;
     }
+
     ///@}
 private:
 
@@ -272,12 +236,9 @@ public:
     /// @param SkinThickness The skin thickness for fattened AABBs, as a fraction
     ///                      of the AABB_base base length.
     /// @param TouchIsOverlap Does touching count as overlapping in query operations?
-    AABB_tree(const TriangleMesh& TriangleMesh, double SkinThickness = 0.001, bool TouchIsOverlap=true) :
-            aabb_base::Tree_base(3, SkinThickness, 16,  TouchIsOverlap)
+    AABB_tree(const TriangleMesh& TriangleMesh ) :
+            aabb_base::Tree_base(3, 0.000001, 16,  false)
     {
-
-        // BaseTreeType::setBoxSize( {2, 2, 10} );
-        // BaseTreeType::setPeriodicity( {false, false, false} );
         const double max_limit = std::numeric_limits<double>::max();
         mLowerBound = {max_limit, max_limit, max_limit};
         mUpperBound = {-max_limit, -max_limit, -max_limit};
@@ -307,12 +268,15 @@ public:
             mLowerBound[2] = std::min<double>(*z_min, mLowerBound[2]);
             mUpperBound[2] = std::max<double>(*z_max, mUpperBound[2]);
         }
-        std::cout << "lower " << mLowerBound[0] << ", " << mLowerBound[1] << ", " << mLowerBound[2] << std::endl;
-        std::cout << "upper " << mUpperBound[0] << ", " << mUpperBound[1] << ", " << mUpperBound[2] << std::endl;
-        std::cout << "AABB_tree is build" << std::endl;
     }
 
-    int count = 0;
+    ///@}
+    ///@name Operations
+    ///@{
+
+    ///@brief Return true if point lies inside outer bounding box.
+    ///@param rPoint
+    ///@return bool
     bool IsWithinBoundingBox(const std::array<double, 3>& rPoint){
         if(   rPoint[0] < mLowerBound[0]
            || rPoint[0] > mUpperBound[0]
@@ -327,8 +291,10 @@ public:
         return true;
     }
 
-    //std::vector<unsigned int> AABB_tree::query(unsigned int particle, const AABB_base& aabb_base)
-    std::vector<unsigned int> query(Ray& rRay)
+    ///@brief Get all interesections of Ray
+    ///@param rRay
+    ///@return std::vector<unsigned int> Holds Id's of triangles.
+    std::vector<unsigned int> Query(Ray& rRay)
     {
         std::vector<unsigned int> stack;
         stack.reserve(256);
@@ -348,7 +314,6 @@ public:
 
             // Test for overlap between the AABBs.
             double t;
-            count++;
             if( rRay.inside(nodeAABB) ){
                 // Check that we're at a leaf node.
                 if (BaseTreeType::Nodes()[node].isLeaf() )
@@ -361,7 +326,7 @@ public:
                     stack.push_back(BaseTreeType::Nodes()[node].right);
                 }
             }
-            if (rRay.intersect(nodeAABB, t) )
+            else if (rRay.intersect(nodeAABB, t) )
             {
                 // Check that we're at a leaf node.
                 if (BaseTreeType::Nodes()[node].isLeaf())
@@ -375,12 +340,13 @@ public:
                 }
             }
         }
-
         return particles;
     }
+    ///@}
 
-    private:
-
+private:
+    ///@name Private Member variables
+    ///@{
     std::array<double,3> mLowerBound{};
     std::array<double,3> mUpperBound{};
     ///@}
