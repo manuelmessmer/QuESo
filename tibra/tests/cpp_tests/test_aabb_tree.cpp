17202a67ddd36cde16df37846ae84b2233674726
manuel.messmer <manuel.messmer@tum.de>
2022-10-14 16:33:31 -0600

    add test.

--- tibra/tests/cpp_tests/test_aabb_tree.cpp
+++ tibra/tests/cpp_tests/test_aabb_tree.cpp
@@ -14,36 +14,44 @@ namespace Testing{
 
 BOOST_AUTO_TEST_SUITE( AABB_treeTestSuite )
 
-BOOST_AUTO_TEST_CASE(RayIntersectionTest) {
+BOOST_AUTO_TEST_CASE(CylinderTriangleIntersectionTest) {
     std::cout << "Testing :: Test AABB Tree :: Ray Intersection" << std::endl;
+
     TriangleMesh triangle_mesh{};
     // Read mesh from STL file
     IO::ReadMeshFromSTL(triangle_mesh, "tibra/tests/cpp_tests/data/cylinder.stl");
 
-    AABB_tree aabb_tree(triangle_mesh);
-    Ray_AABB_primitive ray({0.0, 0.0, 1.0}, {1.0, 1.0, 0.0});
-    auto result = aabb_tree.Query(ray);
+    AABB_tree tree(triangle_mesh);
 
-    std::cout << "Size: " << result.size() << std::endl;
-    for( auto r : result ){
+    TriangleMesh::Vector3d lower_bound = {0, 0, 4.0};
+    TriangleMesh::Vector3d upper_bound = {2, 2, 5.0};
+
+    AABB_primitive aabb(lower_bound, upper_bound);
+    auto results = tree.Query(aabb);
+
+    TriangleMesh new_mesh{};
+
+    std::set<int> indices{};
+    std::map<int,int> index_map{};
+
+    std::vector<IndexType> intersected_triangles{};
+    int count = 0;
+    for( auto r : results){
         const auto& p1 = triangle_mesh.P1(r);
         const auto& p2 = triangle_mesh.P2(r);
         const auto& p3 = triangle_mesh.P3(r);
-
         double t, u, v;
-        if( ray.intersect(p1, p2, p3, t, u, v) ) {
-            std::cout << "id: " << r << std::endl;
-            std::array<double,3> pp = {u, v, 0.0};
-            auto point_global = triangle_mesh.GetPointGlobalSpace(r, pp);
-            std::cout << "t: " << t << " u: " << u  << " v: " << v << std::endl;
-            const double sum = u+v;
-            if( u < 0.0+1e-15 || v < 0.0+1e-15 || sum > 1.0-1e-15 ){
-                std::cout << "on boundary " << std::endl;
-            }
-            std::cout << "x: " << point_global[0] << " y: " << point_global[1]  << " z: " << point_global[2] << std::endl;
+
+        if( aabb.intersect(p1, p2, p3, t, u, v) ){
+            intersected_triangles.push_back(r);
         }
     }
+
+    new_mesh.Copy(intersected_triangles, triangle_mesh);
+    IO::WriteMeshToSTL(new_mesh,"test.stl", true);
+
 }
+
 
 
 BOOST_AUTO_TEST_SUITE_END()
---
+++ tibra/tests/cpp_tests/test_element_classifier.cpp
@@ -0,0 +1,35 @@
+// Author: Manuel Meßmer
+// Email: manuel.messmer@tum.de
+
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+#include <chrono>
+
+#include "geometries/triangle_mesh.h"
+#include "io/io_utilities.h"
+#include "utilities/geometrical_entity_classifier.h"
+
+namespace Testing{
+
+BOOST_AUTO_TEST_SUITE( ElementClassifierTestSuite )
+
+BOOST_AUTO_TEST_CASE(CylinderElementClassifierTest) {
+    std::cout << "Testing :: Test AABB Tree :: Ray Intersection" << std::endl;
+    TriangleMesh triangle_mesh{};
+    // Read mesh from STL file
+    IO::ReadMeshFromSTL(triangle_mesh, "tibra/tests/cpp_tests/data/cylinder.stl");
+
+    GeometricalEntityClassifier classifier(triangle_mesh);
+
+    TriangleMesh::Vector3d lower_bound = {0.0, 0.0, 9.0};
+    TriangleMesh::Vector3d upper_bound = {2.0, 2.0, 11.0};
+    classifier.GetIntersectionState(lower_bound, upper_bound);
+
+
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+} // End namespace Testing
