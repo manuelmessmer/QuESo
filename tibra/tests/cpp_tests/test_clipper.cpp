3fedaec30d306d30f66ef8fbd04ea46b7e5c0d1c
manuel.messmer <manuel.messmer@tum.de>
2022-10-20 16:51:46 -0600

    add first clipper unittest.

--- tibra/tests/cpp_tests/test_clipper.cpp
+++ tibra/tests/cpp_tests/test_clipper.cpp
@@ -6,7 +6,9 @@
 #include <boost/test/unit_test.hpp>
 #include <chrono>
 
-#include "embedding/clipper.h"
+#include "geometries/triangle_mesh.h"
+#include "io/io_utilities.h"
+#include "embedding/brep_operator.h"
 
 namespace Testing{
 
@@ -15,9 +17,28 @@ BOOST_AUTO_TEST_SUITE( ClipperTestSuite )
 BOOST_AUTO_TEST_CASE(ClipperTest1) {
     std::cout << "Testing :: Test Touching Cube 1" << std::endl;
 
-    // Read mesh from STL file
-    // TriangleMesh triangle_mesh{};
-    // IO::ReadMeshFromSTL(triangle_mesh, "tibra/tests/cpp_tests/data/cube_with_cavity.stl");
+    //Read mesh from STL file
+    TriangleMesh triangle_mesh{};
+
+    IO::ReadMeshFromSTL(triangle_mesh, "tibra/tests/cpp_tests/data/cylinder.stl");
+
+    // Build brep_operator
+    BRepOperator brep_operator(triangle_mesh);
+
+    TriangleMesh::Vector3d lower_bound = {0.0, 0.0, 0.0};
+    TriangleMesh::Vector3d upper_bound = {2, 2, 1};
+
+    auto p_clipped_mesh = brep_operator.ClipTriangleMesh(lower_bound, upper_bound);
+
+    p_clipped_mesh->Check();
+
+    double area = 0.0;
+    for( int i = 0; i < p_clipped_mesh->NumOfTriangles(); ++i){
+        area += p_clipped_mesh->Area(i);
+    }
+
+    // Check surface area of clipped mesh.
+    BOOST_CHECK_CLOSE( area, 1.570796327, 1e-2);
 
 }
 
