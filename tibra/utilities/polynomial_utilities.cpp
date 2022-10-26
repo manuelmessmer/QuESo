d22171ba054a7417f0120c5ba76129f579e3f079
manuel.messmer <manuel.messmer@tum.de>
2022-10-26 17:20:07 -0600

    instantiate polys only once.

---
+++ tibra/utilities/polynomial_utilities.cpp
@@ -0,0 +1,6 @@
+// Author: Manuel Meßmer
+// Email: manuel.messmer@tum.de
+
+#include "utilities/polynomial_utilities.h"
+
+Polynomial::Legendre  Polynomial::mLegendre = {Polynomial::p0{}, Polynomial::p1{}, Polynomial::p2{}, Polynomial::p3{}, Polynomial::p4{}, Polynomial::p5{}, Polynomial::p6{}, Polynomial::p7{}, Polynomial::p8{}};
--- tibra/utilities/polynomial_utilities.h
+++ tibra/utilities/polynomial_utilities.h
@@ -179,8 +179,6 @@ private:
     ///@}
 }; // End Class
 
-Polynomial::Legendre  Polynomial::mLegendre = {Polynomial::p0{}, Polynomial::p1{}, Polynomial::p2{}, Polynomial::p3{}, Polynomial::p4{}, Polynomial::p5{}, Polynomial::p6{}, Polynomial::p7{}, Polynomial::p8{}};
-
 ///@}
 
 #endif
