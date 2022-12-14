2c1fe31139c2dd5499caebc6a82fdfd99589bd97
ge69vud <manuel.messmer@tum.de>
2022-12-14 09:58:07 +0100

    update unit test.

--- tibra/tests/cpp_tests/test_generate_boundary_ips.cpp
+++ tibra/tests/cpp_tests/test_generate_boundary_ips.cpp
@@ -20,7 +20,7 @@
 namespace tibra {
 namespace Testing {
 
-BOOST_AUTO_TEST_SUITE( GenerateBoundaryIPsTestSuite )
+BOOST_AUTO_TEST_SUITE( GenerateBoundaryIPsTestElephantSuite )
 
 BOOST_AUTO_TEST_CASE(GenerateBoundaryIPsTest1) {
     std::cout << "Testing :: Prototype :: Generate Boundary Integration Points :: Elephant" << std::endl;
@@ -32,7 +32,7 @@ BOOST_AUTO_TEST_CASE(GenerateBoundaryIPsTest1) {
     Vector3d upper_bound = {0.4, 0.6, 0.35 };
     Parameters parameters( {Component("lower_bound", lower_bound),
                             Component("upper_bound", upper_bound),
-                            Component("min_num_boundary_triangles", 1000UL),
+                            Component("min_num_boundary_triangles", 200UL),
                             Component("number_of_elements", number_of_elements),
                             Component("polynomial_order", Vector3i(2,2,2) ) } );
 
@@ -40,7 +40,6 @@ BOOST_AUTO_TEST_CASE(GenerateBoundaryIPsTest1) {
     IO::ReadMeshFromSTL(triangle_mesh, "tibra/tests/cpp_tests/data/elephant.stl");
 
     // Build brep_operator
-    cgal::CGALBRepOperator cgal_operator_cgal(triangle_mesh, parameters);
     BRepOperator brep_operator(triangle_mesh,parameters);
 
     const double delta_x = 0.1;
@@ -52,6 +51,7 @@ BOOST_AUTO_TEST_CASE(GenerateBoundaryIPsTest1) {
 
     auto start_time = std::chrono::high_resolution_clock::now();
     IndexType number_trimmed_elements = 0;
+
     for(double x = -0.4; x <= 0.4; x += delta_x){
         for(double y = -0.6; y <= 0.6; y += delta_y){
             for(double z = -0.35; z <= 0.35; z += delta_x){
@@ -63,8 +63,9 @@ BOOST_AUTO_TEST_CASE(GenerateBoundaryIPsTest1) {
 
                 Element element(1, local_lower_bound_param, local_upper_bound_param, parameters);
                 if( brep_operator.GetIntersectionState(local_lower_bound, local_upper_bound) == BRepOperator::Trimmed){
-
+                    // Get trimmed domain
                     auto p_trimmed_domain = brep_operator.GetTrimmedDomain(local_lower_bound, local_upper_bound);
+                    // Get boundary integration points
                     auto p_boundary_ips = p_trimmed_domain->pGetBoundaryIps();
 
                     VectorType constant_terms{};
@@ -72,28 +73,27 @@ BOOST_AUTO_TEST_CASE(GenerateBoundaryIPsTest1) {
 
                     // // Read and ignore header
                     getline(file, line);
+
                     double surface_area = 0.0;
                     for( auto& ip : *p_boundary_ips){
                         surface_area += ip.GetWeight();
                     }
+
                     // Read ref surface area
                     getline(file, line);
                     double ref_surface_area = std::stod(line);
-                    BOOST_CHECK_LT(  std::abs(surface_area-ref_surface_area)/ref_surface_area, 1e-10);
+                    BOOST_CHECK_LT(  std::abs(surface_area-ref_surface_area)/std::abs(ref_surface_area), 1e-10);
 
                     double error = 0.0;
                     double norm = 0.0;
                     for( auto& value : constant_terms ){
                         getline(file, line);
                         double ref_value = std::stod(line);
-                        //std::cout << value << ", " << ref_value << std::endl;
-                        error += (value-ref_value)*(value-ref_value);
-                        norm += (ref_value*ref_value);
-                        if( constant_terms[0] < 1e-7){
-                            break;
-                        }
+                        error += std::abs(value-ref_value);
+                        norm += std::abs(ref_value);
                     }
-                    BOOST_CHECK_LT(  std::sqrt(error/norm), 5e-4);
+
+                    BOOST_CHECK_LT( error/norm, 1e-6);
                     number_trimmed_elements++;
                 }
             }
@@ -106,7 +106,7 @@ BOOST_AUTO_TEST_CASE(GenerateBoundaryIPsTest1) {
     std::cout << "TIBRA :: Elapsed Time: " << elapsed_time.count() << std::endl;
 }
 
-BOOST_AUTO_TEST_CASE(GenerateBoundaryIPsTest2) {
+BOOST_AUTO_TEST_CASE(GenerateBoundaryIPsBunnyTest) {
     std::cout << "Testing :: Prototype :: Generate Boundary Integration Points :: Bunny" << std::endl;
 
     typedef boost::numeric::ublas::vector<double> VectorType;
@@ -123,14 +123,13 @@ BOOST_AUTO_TEST_CASE(GenerateBoundaryIPsTest2) {
     IO::ReadMeshFromSTL(triangle_mesh, "tibra/tests/cpp_tests/data/stanford_bunny.stl");
 
     // Build brep_operator
-    cgal::CGALBRepOperator cgal_operator_cgal(triangle_mesh, parameters);
     BRepOperator brep_operator(triangle_mesh, parameters);
 
     const double delta_x = 15;
     const double delta_y = 15;
     const double delta_z = 15;
 
-    auto start_time = std::chrono::high_resolution_clock::now();
+    //auto start_time = std::chrono::high_resolution_clock::now();
 
     std::ifstream file("tibra/tests/cpp_tests/results/surface_integral_bunny.txt");
     std::string line{};
@@ -146,38 +145,44 @@ BOOST_AUTO_TEST_CASE(GenerateBoundaryIPsTest2) {
                 auto local_upper_bound_param = MappingUtilities::FromGlobalToLocalSpace(local_upper_bound, lower_bound, upper_bound);
                 Element element(1, local_lower_bound_param, local_upper_bound_param, parameters);
                 if( brep_operator.GetIntersectionState(local_lower_bound, local_upper_bound) == BRepOperator::Trimmed){
+                    // Get Trimmed domain
                     auto p_trimmed_domain = brep_operator.GetTrimmedDomain(local_lower_bound, local_upper_bound);
-                    const auto& r_clipped_mesh = p_trimmed_domain->GetTriangleMesh();
-                    //IO::WriteMeshToSTL(r_clipped_mesh, "clipped_mesh_before.stl", true);
+                    // Get boundary integration points
                     auto p_boundary_ips = p_trimmed_domain->pGetBoundaryIps();
                     // Read and ignore header
                     getline(file, line);
 
                     double area = 0.0;
-                    for( auto& ip : *p_boundary_ips){
+                    for( const auto& ip : *p_boundary_ips){
                         area += ip.GetWeight();
                     }
                     getline(file, line);
                     const double ref_area = stod(line);
-                    BOOST_CHECK_LT( std::abs(area-ref_area)/ref_area, 1e-10);
+                    BOOST_CHECK_LT( std::abs(area-ref_area)/std::abs(ref_area), 1e-10);
 
                     VectorType constant_terms{};
                     MomentFitting::ComputeConstantTerms(element, p_boundary_ips, constant_terms, parameters);
-                    for( const auto& value : constant_terms){
+
+                    double error = 0.0;
+                    double norm = 0.0;
+                    for( const auto& value : constant_terms ){
                         getline(file, line);
-                        const double ref_value = stod(line);
-                        BOOST_CHECK_LT( std::abs(value-ref_value)/ref_value, 1e-10);
+                        double ref_value = std::stod(line);
+                        error += std::abs(value-ref_value);
+                        norm += std::abs(ref_value);
                     }
+
+                    BOOST_CHECK_LT( error/norm, 1e-6);
                     number_trimmed_elements++;
                 }
             }
         }
     }
     file.close();
-    auto end_time = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed_time = end_time - start_time;
-    std::cout << "TIBRA :: Elapsed Time: " << elapsed_time.count() << std::endl;
-    std::cout << "number_trimmed_elements: " << number_trimmed_elements << std::endl;
+    // auto end_time = std::chrono::high_resolution_clock::now();
+    // std::chrono::duration<double> elapsed_time = end_time - start_time;
+    // std::cout << "TIBRA :: Elapsed Time: " << elapsed_time.count() << std::endl;
+    // std::cout << "number_trimmed_elements: " << number_trimmed_elements << std::endl;
 }
 
 
