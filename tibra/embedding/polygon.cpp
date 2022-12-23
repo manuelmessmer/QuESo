84ab884c7fa8306ceb981d044d31d74b246f6e5f
ge69vud <manuel.messmer@tum.de>
2022-12-23 13:59:53 +0100

    update polygon.

--- tibra/embedding/polygon.cpp
+++ tibra/embedding/polygon.cpp
@@ -9,12 +9,31 @@ namespace tibra {
 
 // Function Definitions Polygon
 template<IndexType SIZE>
-void Polygon<SIZE>::AddVertex(const PointType& rPoint) {
+IndexType Polygon<SIZE>::AddVertex(const PointType& rPoint ) {
+    if( mNumVertices >= SIZE ){
+        throw std::runtime_error("Polygon :: AddVertex :: Size of Polygon is exceeded.");
+    }
+    mVertices[mNumVertices].first = rPoint;
+    return mNumVertices++;
+}
+
+template<IndexType SIZE>
+IndexType Polygon<SIZE>::AddVertex(const PointType& rPoint, const std::array<bool, 6>& rStatus) {
+    if( mNumVertices >= SIZE ){
+        throw std::runtime_error("Polygon :: AddVertex :: Size of Polygon is exceeded.");
+    }
+    mVertices[mNumVertices].first = rPoint;
+    mVertices[mNumVertices].second = rStatus;
+    return mNumVertices++;
+}
+
+template<IndexType SIZE>
+IndexType Polygon<SIZE>::AddVertex(const VertexType& rPoint) {
     if( mNumVertices >= SIZE ){
         throw std::runtime_error("Polygon :: AddVertex :: Size of Polygon is exceeded.");
     }
     mVertices[mNumVertices] = rPoint;
-    ++mNumVertices;
+    return mNumVertices++;
 }
 
 template<IndexType SIZE>
@@ -23,7 +42,7 @@ IndexType Polygon<SIZE>::NumVertices() const {
 }
 
 template<IndexType SIZE>
-const PointType& Polygon<SIZE>::GetVertex(IndexType i) const {
+const typename Polygon<SIZE>::VertexType& Polygon<SIZE>::GetVertex(IndexType i) const {
     if( i >= mNumVertices ){
         throw std::runtime_error("Polygon :: GetVertex :: Size of Polygon is exceeded.");
     }
@@ -31,98 +50,111 @@ const PointType& Polygon<SIZE>::GetVertex(IndexType i) const {
 }
 
 template<IndexType SIZE>
-const PointType& Polygon<SIZE>::operator[] (IndexType i) const {
+const typename Polygon<SIZE>::VertexType& Polygon<SIZE>::operator[] (IndexType i) const {
+    if( i >= mNumVertices ){
+        throw std::runtime_error("Polygon :: GetVertex :: Size of Polygon is exceeded.");
+    }
     return mVertices[i];
 }
 
 template<IndexType SIZE>
-std::unique_ptr<TriangleMesh> Polygon<SIZE>::pGetTriangleMesh() const {
+const typename Polygon<SIZE>::VertexType& Polygon<SIZE>::GetLastVertex() const{
+    return mVertices[mNumVertices-1];
+}
 
+template<IndexType SIZE>
+void Polygon<SIZE>::Clear(){
+    const std::array<bool,6> points_on_plane = {false, false, false, false, false, false};
+    std::fill(mVertices.begin(), mVertices.end(), std::make_pair(PointType{}, points_on_plane) );
+    mNumVertices = 0;
+}
+
+template<IndexType SIZE>
+std::unique_ptr<TriangleMesh> Polygon<SIZE>::pGetTriangleMesh() const {
     auto p_new_mesh = std::make_unique<TriangleMesh>();
     p_new_mesh->Reserve(mNumVertices);
 
     if(mNumVertices < 3){
-        throw std::runtime_error("Clipper :: pGetTriangleMesh :: mNumVertices < 3.");
+        return std::move(p_new_mesh);
     }
 
     if( mNumVertices == 3 ){
-
-        p_new_mesh->AddVertex( mVertices[0] );
-        p_new_mesh->AddVertex( mVertices[1] );
-        p_new_mesh->AddVertex( mVertices[2] );
+        p_new_mesh->AddVertex( mVertices[0].first );
+        p_new_mesh->AddVertex( mVertices[1].first );
+        p_new_mesh->AddVertex( mVertices[2].first );
 
         p_new_mesh->AddTriangle( {0, 1, 2} );
         p_new_mesh->AddNormal( mNormal );
+
+        // Add edges, that are located on a plane, to the mesh.
+        // Planes: (-x, +x, -y, y, -z, z)
+        for( IndexType plane_index = 0; plane_index < 6; ++plane_index ){
+            const bool v1_on_plane = mVertices[0].second[plane_index];
+            const bool v2_on_plane = mVertices[1].second[plane_index];
+            const bool v3_on_plane = mVertices[2].second[plane_index];
+            if( (v1_on_plane + v2_on_plane + v3_on_plane) == 3 ) {
+                throw std::runtime_error("Polygon :: pGetTriangleMesh :: All vertices are set on plane.");
+            }
+            if( v1_on_plane && v2_on_plane ){
+                p_new_mesh->AddEdgeOnPlane(plane_index, 0, 1, 0);
+            }
+            else if( v2_on_plane && v3_on_plane ){
+                p_new_mesh->AddEdgeOnPlane(plane_index, 1, 2, 0);
+            }
+            else if( v3_on_plane && v1_on_plane ){
+                p_new_mesh->AddEdgeOnPlane(plane_index, 2, 0, 0);
+            }
+        }
 
         return std::move(p_new_mesh);
     }
 
     // Compute mean of vertices
     PointType centroid = {0.0, 0.0, 0.0};
-    const double inv_num_vertices = 1.0/mNumVertices;
-
     for( IndexType i = 0 ; i < mNumVertices; ++i){
-        centroid[0] += inv_num_vertices*mVertices[i][0];
-        centroid[1] += inv_num_vertices*mVertices[i][1];
-        centroid[2] += inv_num_vertices*mVertices[i][2];
+        centroid += mVertices[i].first;
     }
+    centroid /= mNumVertices;
 
     IndexType vertex_count = 0;
+    IndexType triangle_count = 0;
     for( IndexType i = 0 ; i < mNumVertices-1; ++i){
-        p_new_mesh->AddVertex( mVertices[i] );
-        p_new_mesh->AddVertex( mVertices[i+1] );
+        p_new_mesh->AddVertex( mVertices[i].first );
+        p_new_mesh->AddVertex( mVertices[i+1].first );
         p_new_mesh->AddVertex( centroid );
 
         p_new_mesh->AddTriangle( {vertex_count+0, vertex_count+1, vertex_count+2} );
         p_new_mesh->AddNormal( mNormal );
+
+        for( IndexType plane_index = 0; plane_index < 6; ++plane_index ){
+            const bool v1_on_plane = mVertices[i].second[plane_index];
+            const bool v2_on_plane = mVertices[i+1].second[plane_index];
+            if( v1_on_plane && v2_on_plane ){
+                p_new_mesh->AddEdgeOnPlane(plane_index, vertex_count+0, vertex_count+1, triangle_count);
+            }
+        }
+        ++triangle_count;
         vertex_count += 3;
     }
 
-    p_new_mesh->AddVertex( mVertices[mNumVertices-1] );
-    p_new_mesh->AddVertex( mVertices[0] );
+    p_new_mesh->AddVertex( mVertices[mNumVertices-1].first );
+    p_new_mesh->AddVertex( mVertices[0].first );
     p_new_mesh->AddVertex( centroid );
 
     p_new_mesh->AddTriangle( {vertex_count+0, vertex_count+1, vertex_count+2 } );
     p_new_mesh->AddNormal( mNormal );
 
+    for( IndexType plane_index = 0; plane_index < 6; ++plane_index ){
+        const bool v1_on_plane = mVertices[mNumVertices-1].second[plane_index];
+        const bool v2_on_plane = mVertices[0].second[plane_index];
+        if( v1_on_plane && v2_on_plane ){
+            p_new_mesh->AddEdgeOnPlane(plane_index, vertex_count+0, vertex_count+1, triangle_count);
+        }
+    }
+
     return std::move(p_new_mesh);
 }
 
-template<IndexType SIZE>
-const PointType& Polygon<SIZE>::GetLastVertex() const{
-    return mVertices[mNumVertices-1];
-}
-
-template<IndexType SIZE>
-void Polygon<SIZE>::Clear(){
-    std::fill(mVertices.begin(), mVertices.end(), PointType{});
-    mNumVertices = 0;
-}
-
-
-
-template<IndexType SIZE>
-std::unique_ptr<typename Polygon<SIZE>::EdgesType> Polygon<SIZE>::pGetEdgesOnPlane(IndexType PlaneIndex, double Position, double PlaneThickness) const {
-
-    auto p_new_edges = std::make_unique<EdgesType>();
-
-    if(mNumVertices < 3){
-        throw std::runtime_error("Obacht");
-    }
-
-    for( IndexType i = 0 ; i < mNumVertices-1; ++i){
-        if( std::abs((mVertices[i][PlaneIndex] - Position)) < PlaneThickness
-                && std::abs((mVertices[i+1][PlaneIndex] - Position)) < PlaneThickness ){
-            p_new_edges->push_back( {mVertices[i], mVertices[i+1]} );
-        }
-    }
-    if( std::abs((mVertices[mNumVertices-1][PlaneIndex] - Position)) < PlaneThickness
-            && std::abs((mVertices[0][PlaneIndex] - Position)) < PlaneThickness ){
-        p_new_edges->push_back( {mVertices[mNumVertices-1], mVertices[0]} );
-    }
-
-    return std::move(p_new_edges);
-}
 
 // Explicit instantiation Polygon
 template class Polygon<9>;
--- tibra/embedding/polygon.h
+++ tibra/embedding/polygon.h
@@ -26,17 +26,45 @@ class Polygon {
 public:
     ///@name Type Definitions
     ///@{
-    typedef std::vector<std::array<PointType, 2>> EdgesType;
 
+    /// VertexType: Stores PointType and array, that holds information whether or not a points lies on a plane.
+    /// Mapping (-x, x, -y, y, -z, z).
+    typedef std::pair<PointType, std::array<bool,6> > VertexType;
+
+    ///@}
+    ///@name Life Cycle
+    ///@{
+
+    /// Constructor
     Polygon(const PointType& rNormal) : mNormal(rNormal){}
 
     ///@}
     ///@name Operations
     ///@{
 
+    ///@brief Adds Point to Polygon. Note that this function sets std::array<bool,6> = false.
+    ///       Only use this function, if point on plane information is not required.
+    ///@param rPoint PointType
+    ///@return Index of added vertex.
+    IndexType AddVertex(const PointType& rPoint);
+
     ///@brief Adds Vertex to Polygon
     ///@param rPoint
-    void AddVertex(const PointType& rPoint);
+    ///@param rIsOnPlanes (-x, +x, -y, y, -z, z) True, if point is on one of respective planes.
+    ///@return Index of added vertex.
+    IndexType AddVertex(const PointType& rPoint, const std::array<bool, 6>& rIsOnPlanes);
+
+    ///@brief Adds Vertex to Polygon
+    ///@param VertexType
+    ///@return Index of added vertex.
+    IndexType AddVertex(const VertexType& rPoint);
+
+    /// @brief Define point to be on plane.
+    /// @param PointIndex
+    /// @param PlaneIndex (-x, x, -y, y, -z, z)
+    void SetIndexOnPlane(IndexType PointIndex, IndexType PlaneIndex) {
+        mVertices[PointIndex].second[PlaneIndex] = true;
+    }
 
     ///@brief Return current number of vertices.
     ///@return IndexType
@@ -44,24 +72,21 @@ public:
 
     ///@brief Get i-th vertex.
     ///@param i Index.
-    ///@return const PointType&
-    const PointType& GetVertex(IndexType i) const;
+    ///@return const VertexType&
+    const VertexType& GetVertex(IndexType i) const;
 
     ///@brief Get i-th vertex.
     ///@param i Index.
-    ///@return const PointType&
-    const PointType& operator[] (IndexType i) const;
+    ///@return const VertexType&
+    const VertexType& operator[] (IndexType i) const;
 
     ///@brief Get last vertex.
-    const PointType& GetLastVertex() const;
+    ///@return const VertexType&
+    const VertexType& GetLastVertex() const;
 
-    ///@brief Triangulates polygon centroid coodinate and returns triangles. Centroid is computed as mean of all vertices.
-    ///@param rNormal Provide normal to avoid recomputation.
+    ///@brief Triangulates polygon based on centroide and returns triangles. Centroid is computed as mean of all vertices.
     ///@return std::unique_ptr<TriangleMesh> Contains vertices of triangles.
     std::unique_ptr<TriangleMesh> pGetTriangleMesh() const;
-
-    ///@brief GetBoundaryEdges
-    std::unique_ptr<EdgesType> pGetEdgesOnPlane(IndexType PlaneIndex, double Position, double PlaneThickness) const;
 
     ///@brief Clears vertex container of polygon.
     void Clear();
@@ -70,7 +95,7 @@ public:
 private:
     ///@name Member variables
     ///@{
-    std::array<PointType, SIZE> mVertices{}; // Keep static array to be fast.
+    std::array<VertexType, SIZE> mVertices{}; // Keep static array to be fast.
     IndexType mNumVertices = 0;
     PointType mNormal{};
     ///@}
@@ -79,4 +104,4 @@ private:
 ///@} End TIBRA classes
 
 } // End namespace tibra
-#endif
+#endif // POLYGON_INCLUDE_H
