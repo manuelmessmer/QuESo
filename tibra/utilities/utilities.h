73bfe46c8be68e14bf016feec973e0c04731d852
ge69vud <manuel.messmer@tum.de>
2022-12-30 13:39:39 +0100

    add power to utilities.

--- tibra/utilities/utilities.h
+++ tibra/utilities/utilities.h
@@ -8,6 +8,10 @@ namespace tibra {
 
 namespace utilities {
 
+    /// @brief Swap function for two ptrs.
+    /// @tparam T Type.
+    /// @param a Pointer 1.
+    /// @param b Pointer 2.
     template <typename T>
     static inline void swap(T& a, T& b) {
         T tmp = a;
@@ -15,11 +19,28 @@ namespace utilities {
         b = tmp;
     }
 
+    /// @brief Swap function for two unique ptrs.
+    /// @tparam T Type
+    /// @param a Pointer 1.
+    /// @param b Poitner 2.
     template <typename T>
     static inline void swap_unique(T& a, T& b) {
         T tmp = std::move(a);
         a = std::move(b);
         b = std::move(tmp);
+    }
+
+    ///@brief Simple Power functions
+    ///@param x Value
+    ///@param p Order
+    ///@details For gcc (without --ffast-math compiler flag) this is faster than std::pow().
+    static double inline power( double x, std::size_t p){
+        double result = 1.0;
+        while( p > 0UL ) {
+            result = result * x;
+            p -= 1;
+        }
+        return result;
     }
 
 } // End namespace utiliites
