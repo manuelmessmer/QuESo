5e8a02ab45ad6d6dec26021b993ced9a2713b42a
ge69vud <manuel.messmer@tum.de>
2022-12-30 16:12:49 +0100

    add example.

---
+++ examples/stanford_bunny/TIBRA_main.py
@@ -0,0 +1,9 @@
+# Project imports
+from TIBRA_PythonApplication.PyTIBRA import PyTIBRA
+
+def main():
+    pytibra = PyTIBRA("TIBRAParameters.json")
+    pytibra.Run()
+
+if __name__ == "__main__":
+    main()
