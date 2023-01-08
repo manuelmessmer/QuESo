f18ba8a86a737ebcda84471d9d17a77dd6d5180f
manuel.messmer <manuel.messmer@tum.de>
2023-01-08 03:44:16 -0700

    rename triangle_gauss_points.hpp.

--- tibra/containers/triangle_mesh.hpp
+++ tibra/containers/triangle_mesh.hpp
@@ -14,7 +14,7 @@
 //// Project includes
 #include "define.hpp"
 #include "utilities/math_utilities.hpp"
-#include "containers/triangle_gauss_legendre_integration_points.h"
+#include "containers/triangle_gauss_legendre_integration_points.hpp"
 #include "containers/boundary_integration_point.h"
 
 namespace tibra {
