dcc91f7ae127b4dde47a75f828132c8b2e9ce168
ge69vud <manuel.messmer@tum.de>
2023-01-23 14:59:09 +0100

    First working octree.

--- tibra/embedding/octree.h
+++ tibra/embedding/octree.h
@@ -9,6 +9,7 @@
 
 //// Project includes
 #include "embedding/trimmed_domain_base.h"
+#include "quadrature/single_element.h"
 #include "define.hpp"
 
 namespace tibra {
@@ -16,17 +17,32 @@ namespace tibra {
 ///@name TIBRA Classes
 ///@{
 
-class Node {
-    public:
-        Node( const PointType& rLowerBound, const PointType& rUpperBound, IndexType Level = 0UL) :
-            mLowerBound(rLowerBound), mUpperBound(rUpperBound), mLevel(Level)
-        {
-            mNumChildren = 0;
-        }
 
-        void Refine(IndexType MinLevel, IndexType MaxLevel){
-            if( this->IsLeaf() ){
-                if( mLevel < MinLevel ){
+
+/**
+ * @class  Octree
+ * @author Manuel Messmer
+ * @tparam TOperator: Required member operations:
+ *                   -GetIntersectionState(const PointType& rLowerBound, const PointType& rUpperBound)
+ *                   -IsInside(const PointType& rPoint)
+*/
+template<typename TOperator>
+class Octree {
+private:
+    typedef std::vector<IntegrationPoint> IntegrationPointVectorType;
+    typedef Unique<IntegrationPointVectorType> IntegrationPointVectorPtrType;
+
+    class Node {
+        public:
+            Node( const PointType& rLowerBound, const PointType& rUpperBound, IntersectionStatusType Status, IndexType Level = 0UL) :
+                mLowerBound(rLowerBound), mUpperBound(rUpperBound), mStatus(Status), mLevel(Level)
+            {
+                mChildren = {nullptr};
+                mNumChildren = 0;
+            }
+
+            void Refine(IndexType MinLevel, IndexType MaxLevel, TOperator* pOperator){
+                if( this->IsLeaf() ){
                     const auto delta = (mUpperBound - mLowerBound) * 0.5;
                     const PointType delta_x(delta[0], 0.0, 0.0);
                     const PointType delta_y(0.0, delta[1], 0.0);
@@ -40,72 +56,122 @@ class Node {
                     //    |________|/
                     //    e        f
                     //
-                    mChildren[0] = MakeUnique<Node>(mLowerBound, mLowerBound+delta);                 // Corner a
-                    mChildren[1] = MakeUnique<Node>(mLowerBound+delta_x, mLowerBound+delta+delta_x); // Corner b (a+delta_x)
-                    mChildren[2] = MakeUnique<Node>(mUpperBound-delta-delta_z, mUpperBound-delta_z); // Corner c (g-delta_z)
-                    mChildren[3] = MakeUnique<Node>(mLowerBound+delta_y, mLowerBound+delta+delta_y); // Corner d (a+delta_y)
-                    mChildren[4] = MakeUnique<Node>(mLowerBound+delta_z, mLowerBound+delta+delta_z); // Corner e (a+delta_z)
-                    mChildren[5] = MakeUnique<Node>(mUpperBound-delta-delta_z, mUpperBound-delta_z); // Corner f (g-delta_z)
-                    mChildren[7] = MakeUnique<Node>(mUpperBound-delta, mUpperBound);                 // Corner g
-                    mChildren[6] = MakeUnique<Node>(mUpperBound-delta-delta_x, mUpperBound-delta_x); // Corner h (g-delta_x)
+                    if( (mLevel < MinLevel) || (mLevel < MaxLevel && mStatus == IntersectionStatus::Trimmed) ){
+                        CreateNewNode(MinLevel, MaxLevel, 0, mLowerBound, mLowerBound+delta, pOperator);                 // Corner a
+                        CreateNewNode(MinLevel, MaxLevel, 1, mLowerBound+delta_x, mLowerBound+delta+delta_x, pOperator); // Corner b (a+delta_x)
+                        CreateNewNode(MinLevel, MaxLevel, 2, mUpperBound-delta-delta_z, mUpperBound-delta_z, pOperator); // Corner c (g-delta_z)
+                        CreateNewNode(MinLevel, MaxLevel, 3, mLowerBound+delta_y, mLowerBound+delta+delta_y, pOperator); // Corner d (a+delta_y)
+                        CreateNewNode(MinLevel, MaxLevel, 4, mLowerBound+delta_z, mLowerBound+delta+delta_z, pOperator); // Corner e (a+delta_z)
+                        CreateNewNode(MinLevel, MaxLevel, 5, mUpperBound-delta-delta_y, mUpperBound-delta_y, pOperator); // Corner f (g-delta_y)
+                        CreateNewNode(MinLevel, MaxLevel, 6, mUpperBound-delta, mUpperBound, pOperator);                 // Corner g
+                        CreateNewNode(MinLevel, MaxLevel, 7, mUpperBound-delta-delta_x, mUpperBound-delta_x, pOperator); // Corner h (g-delta_x)
+                    }
                 }
+                else {
+                    for( IndexType i = 0; i < 8; ++i){
+                        if( mChildren[i] ){
+                            mChildren[i]->Refine(MinLevel, MaxLevel, pOperator);
+                        }
+                    }
+                }
+
             }
-            else {
-                for( IndexType i = 0; i < 8; ++i){
-                    if( mChildren[i] ){
-                        mChildren[i]->Refine(MinLevel, MaxLevel);
+
+            void GetIntegrationPoints(IntegrationPointVectorType* pPoints, const PointType& GlobalLowerBound, const PointType& GlobalUpperBound, TOperator* pOperator) {
+                if( this->IsLeaf() ){
+                    auto lower_bound_param = Mapping::GlobalToParam(mLowerBound, GlobalLowerBound, GlobalUpperBound);
+                    auto upper_bound_param = Mapping::GlobalToParam(mUpperBound, GlobalLowerBound, GlobalUpperBound);
+                    IntegrationPointVectorType r_integration_points{};
+                    SingleElement::AssembleIPs(r_integration_points, lower_bound_param, upper_bound_param, {2Ul, 2Ul, 2Ul});
+                    for( auto& point : r_integration_points){
+                        auto tmp_point = Mapping::ParamToGlobal(point, GlobalLowerBound, GlobalUpperBound);
+                        if( pOperator->IsInsideTrimmedDomain( tmp_point ) ){
+                            pPoints->push_back(point);
+                        }
+                    }
+                } else {
+                    for( IndexType i = 0; i < 8; ++i){
+                        if( mChildren[i] ){
+                            mChildren[i]->GetIntegrationPoints(pPoints, GlobalLowerBound, GlobalUpperBound, pOperator);
+                        }
                     }
                 }
             }
-        }
 
-        void Volume(double& volume){
-            if( IsLeaf() ){
-                const auto delta = (mUpperBound - mLowerBound);
-                volume += delta[0]*delta[1]*delta[2];
-            } else {
-                for( IndexType i = 0; i < 8; ++i){
-                    if( mChildren[i] ){
-                        mChildren[i]->Volume(volume);
+            void Count(IndexType& rValue) {
+                if( this->IsLeaf() ){
+                    ++rValue;
+                } else {
+                    for( IndexType i = 0; i < 8; ++i){
+                        if( mChildren[i] ){
+                            mChildren[i]->Count(rValue);
+                        }
                     }
                 }
             }
-        }
 
-        bool IsLeaf(){
-            return (mNumChildren == 0);
-        }
+            void Volume(double& volume){
+                if( IsLeaf() ){
+                    const auto delta = (mUpperBound - mLowerBound);
+                    volume += delta[0]*delta[1]*delta[2];
+                    TIBRA_INFO << delta[0]*delta[1]*delta[2] << std::endl;
+                } else {
+                    for( IndexType i = 0; i < 8; ++i){
+                        if( mChildren[i] ){
+                            mChildren[i]->Volume(volume);
+                        }
+                    }
+                }
+            }
 
-        IndexType Level(){
-            return mLevel;
-        }
+        private:
+            void CreateNewNode(IndexType MinLevel, IndexType MaxLevel, IndexType i, const PointType& rLowerBound, const PointType& rUpperBound, TOperator* pOperator){
+                const auto status = pOperator->GetIntersectionState(rLowerBound, rUpperBound);
+                if( status != IntersectionStatus::Outside ){
+                    mChildren[i] = MakeUnique<Node>(rLowerBound, rUpperBound, status, mLevel+1);
+                    ++mNumChildren;
+                    mChildren[i]->Refine(MinLevel, MaxLevel, pOperator);
+                }
+            }
 
-    private:
-        PointType mLowerBound;
-        PointType mUpperBound;
-        std::array<Unique<Node>, 8> mChildren{};
-        SizeType mLevel;
-        SizeType mNumChildren;
-};
+            bool IsLeaf(){
+                return (mNumChildren == 0);
+            }
 
-/**
- * @class  Octree
- * @author Manuel Messmer
-*/
-class Octree {
+            PointType mLowerBound;
+            PointType mUpperBound;
+            IntersectionStatus mStatus;
+            std::array<Unique<Node>, 8> mChildren{};
+            SizeType mLevel;
+            SizeType mNumChildren;
+    };
 
 public:
     ///@name Type Definitions
     ///@{
 
-    Octree(TrimmedDomainBase* pTrimmedDomain) : mpTrimmedDomain(pTrimmedDomain)
+    Octree(TOperator* pOperator, const PointType& rLowerBound, const PointType& rUpperBound, const Parameters& rParameters)
+        : mpOperator(pOperator), mrParameters(rParameters)
     {
-        const auto bounding_box = pTrimmedDomain->GetBoundingBoxOfTrimmedDomain();
-        mpRoot = MakeUnique<Node>(bounding_box.first, bounding_box.second);
+        mpRoot = MakeUnique<Node>(rLowerBound, rUpperBound, IntersectionStatus::Trimmed);
     }
 
     void Refine(IndexType MinLevel, IndexType MaxLevel){
-        mpRoot->Refine(MinLevel, MaxLevel);
+        mpRoot->Refine(MinLevel, MaxLevel, mpOperator);
+    }
+
+    SizeType NumberOfLeafs(){
+        SizeType number_of_leafs = 0UL;
+        mpRoot->Count(number_of_leafs);
+        return number_of_leafs;
+
+    }
+
+    IntegrationPointVectorPtrType GetIntegrationPoints(){
+        auto p_points = MakeUnique<IntegrationPointVectorType>();
+        p_points->reserve(NumberOfLeafs());
+        mpRoot->GetIntegrationPoints(p_points.get(), mrParameters.LowerBound(), mrParameters.UpperBound(), mpOperator );
+        return std::move(p_points);
     }
 
     double Volume(){
@@ -116,11 +182,11 @@ public:
 
 private:
 
-
-    TrimmedDomainBase* mpTrimmedDomain;
     Unique<Node> mpRoot;
+    const Parameters& mrParameters;
 
 protected:
+    TOperator* mpOperator;
 
 };
 
