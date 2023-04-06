83abc579f128a2b816689fe8363335cd5b6a5b46
ge69vud <manuel.messmer@tum.de>
2023-04-06 23:02:23 +0200

    Try.

--- .github/workflows/configure.cmd
+++ .github/workflows/configure.cmd
@@ -10,7 +10,7 @@ set APP_BUILD=%APP_SOURCE%\..\build
 
 rem Set basic configuration
 set CMAKE_BUILD_TYPE=Release
-set BOOST_ROOT=%TEMP%\boost\boost_1_81_0
+set BOOST_ROOT=D:\TEMP\boost\boost_1_81_0
 
 rem rem Clean
 del /F /Q "%APP_BUILD%\%CMAKE_BUILD_TYPE%\cmake_install.cmake"
@@ -21,7 +21,7 @@ rem Configure
 @echo on
 cmake -G"Visual Studio 17 2022" -A x64 -H"%APP_SOURCE%" -B"%APP_BUILD%\%CMAKE_BUILD_TYPE%"  ^
 -DUSE_CGAL=OFF                                                                       ^
--DBOOST_ROOT="%TEMP%\boost\boost_1_81_0"                                              ^
+-DBOOST_ROOT="D:\TEMP\boost\boost_1_81_0"                                              ^
 -DCGAL_DIR="..."
 
 rem Build
