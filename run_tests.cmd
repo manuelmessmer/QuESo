fcb338813208727e47663672d21b566c1b6307fb
ge69vud <manuel.messmer@tum.de>
2023-04-12 17:14:33 +0200

    Add testrunner for windows.

---
+++ run_tests.cmd
@@ -0,0 +1,8 @@
+@echo off
+set arg1=%1
+
+IF "%arg1%" == "all" ( call py tibra/tests/test_tibra.py )
+IF "%arg1%" == "py" ( call py tibra/tests/test_tibra.py )
+
+IF "%arg1%" == "all" ( call "TestExecutables/Release/test_tibra.exe" )
+IF "%arg1%" == "cpp" ( call "TestExecutables/Release/test_tibra.exe" )
