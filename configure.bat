dac1a5b4de61f585617fe7fa873ce94e114a26b0
ge69vud <manuel.messmer@tum.de>
2023-04-12 16:58:56 +0200

    Update configrue files.

--- configure.bat
+++ configure.bat
@@ -18,11 +18,15 @@ del /F /Q "%APP_BUILD%\%CMAKE_BUILD_TYPE%\cmake_install.cmake"
 del /F /Q "%APP_BUILD%\%CMAKE_BUILD_TYPE%\CMakeCache.txt"
 del /F /Q "%APP_BUILD%\%CMAKE_BUILD_TYPE%\CMakeFiles"
 
+rem Enable this if your build is slow and you have a multi-core machine
+rem set TIBRA_PARALLEL_BUILD_FLAG=/MP4
+
 rem Configure
 @echo on
 cmake -G"Visual Studio 17 2022" -A x64 -H"%APP_SOURCE%" -B"%APP_BUILD%\%CMAKE_BUILD_TYPE%"  ^
--DUSE_CGAL=OFF                                                                       ^
--DCGAL_DIR="..."
+-DUSE_CGAL=OFF                                                                              ^
+-DCGAL_DIR="..."                                                                            ^
+-DCMAKE_CXX_FLAGS="%TIBRA_PARALLEL_BUILD_FLAG%"
 
 rem Build
 cmake --build "%APP_BUILD%/%CMAKE_BUILD_TYPE%" --target install -- /property:configuration=%CMAKE_BUILD_TYPE% /p:Platform=x64
--- configure.sh
+++ configure.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-# Please do not modify this script
 
-# Change directory to tibra/
-cd "tibra/"
-# Set compiler
-#export CC=gcc-10
-#export CXX=g++-10
+# Set compiler (optional)
+export CC=gcc
+export CXX=g++
 
-export APP_SOURCE="${APP_SOURCE:-"$( cd "$(dirname "$0")" ; pwd -P )"}"
+export APP_SOURCE="${APP_SOURCE:-"$( cd "$(dirname "$0")" ; pwd -P )"}/tibra"
 export APP_BUILD="${APP_SOURCE}/../build/"
 
 # Set basic configuration
@@ -15,10 +12,9 @@ export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-"Release"}
 export PYTHON_EXECUTABLE=${PYTHON_EXECUTABLE:-"/usr/bin/python3"}
 
 # Clean
-#clear
-#rm -rf "${APP_BUILD}/${CMAKE_BUILD_TYPE}/cmake_install.cmake"
-#rm -rf "${APP_BUILD}/${CMAKE_BUILD_TYPE}/CMakeCache.txt"
-#rm -rf "${APP_BUILD}/${CMAKE_BUILD_TYPE}/CMakeFiles"
+clear
+rm -rf "${APP_BUILD}/${CMAKE_BUILD_TYPE}/cmake_install.cmake"
+rm -rf "${APP_BUILD}/${CMAKE_BUILD_TYPE}/CMakeCache.txt"
 
 # Configure
 cmake -H"${APP_SOURCE}" -B"${APP_BUILD}/${CMAKE_BUILD_TYPE}" \
