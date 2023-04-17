2d5e83d9b8e1c23f86e1886c3e80221ba522c711
ge69vud <manuel.messmer@tum.de>
2023-04-17 10:10:52 +0200

    Update test runner.

--- run_tests.cmd
+++ run_tests.cmd
@@ -6,3 +6,5 @@ IF "%arg1%" == "py" ( call py tibra/tests/test_tibra.py )
 
 IF "%arg1%" == "all" ( call "TestExecutables/Release/test_tibra.exe" )
 IF "%arg1%" == "cpp" ( call "TestExecutables/Release/test_tibra.exe" )
+
+IF "%arg1%" == "thingi10k" ( call "TestExecutables/Release/test_tibra_thingi10k.exe -- all %arg2% 5 50" )
