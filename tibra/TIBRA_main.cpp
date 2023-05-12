bfa22b4c4422d7525941263d5fcbb656f7f72493
ge69vud <manuel.messmer@tum.de>
2023-05-12 09:46:44 +0200

    Add flodd_fill.

--- tibra/TIBRA_main.cpp
+++ tibra/TIBRA_main.cpp
@@ -18,17 +18,9 @@ namespace tibra {
 void TIBRA::Run(){
 
   // Get extreme points of bounding box
-  const auto lower_bound = mParameters.LowerBound();
-  const auto upper_bound = mParameters.UpperBound();
-
   const IndexType number_elements_x = mParameters.NumberOfElements()[0];
   const IndexType number_elements_y = mParameters.NumberOfElements()[1];
   const IndexType number_elements_z = mParameters.NumberOfElements()[2];
-
-  // Obtain discretization of background mesh.
-  const double delta_x = std::abs(lower_bound[0] - upper_bound[0])/(number_elements_x);
-  const double delta_y = std::abs(lower_bound[1] - upper_bound[1])/(number_elements_y);
-  const double delta_z = std::abs(lower_bound[2] - upper_bound[2])/(number_elements_z);
 
   const IndexType global_number_of_elements = number_elements_x * number_elements_y * number_elements_z;
   mpElementContainer->reserve(global_number_of_elements);
@@ -38,32 +30,29 @@ void TIBRA::Run(){
   double et_compute_intersection = 0.0;
   double et_moment_fitting = 0.0;
 
-  #pragma omp parallel for reduction(+ : et_compute_intersection) reduction(+ : et_check_intersect) reduction(+ : et_moment_fitting) schedule(dynamic)
-  for( int i = 0; i < static_cast<int>(global_number_of_elements); ++i){
-    // Unroll 'for' loop to enable better parallelization.
-    // First walk along rows (x), then columns (y) then into depths (z).
-    const IndexType index_in_row_column_plane = i % (number_elements_x*number_elements_y);
-    const IndexType xx = index_in_row_column_plane % number_elements_x; // row
-    const IndexType yy = index_in_row_column_plane / number_elements_x; // column
-    const IndexType zz = i / (number_elements_x*number_elements_y);     // depth
+  Unique<BRepOperatorBase::StatusVectorType> p_classifications = nullptr;
+  if( mParameters.Get<bool>("embedding_flag") ){
+      Timer timer_check_intersect{};
+      p_classifications = mpBRepOperator->GetElementClassifications();
+      et_check_intersect += timer_check_intersect.Measure();
+  }
 
-    // Construct bounding box for each element
-    const PointType el_lower_bound{lower_bound[0] + (xx)*delta_x, lower_bound[1] + (yy)*delta_y, lower_bound[2] + (zz)*delta_z};
-    const PointType el_upper_bound{lower_bound[0] + (xx+1)*delta_x, lower_bound[1] + (yy+1)*delta_y, lower_bound[2] + (zz+1)*delta_z };
+  #pragma omp parallel for reduction(+ : et_compute_intersection) reduction(+ : et_check_intersect) reduction(+ : et_moment_fitting) schedule(dynamic)
+  for( int index = 0; index < static_cast<int>(global_number_of_elements); ++index){
+
+    const auto bounding_box = mMapper.GetBoundingBoxFromIndex(index);
 
     // Map points into parametric/local space
-    const auto el_lower_bound_param = Mapping::PointFromGlobalToParam(el_lower_bound, lower_bound, upper_bound);
-    const auto el_upper_bound_param = Mapping::PointFromGlobalToParam(el_upper_bound, lower_bound, upper_bound);
+    const auto bb_lower_bound_param = mMapper.PointFromGlobalToParam(bounding_box.first);
+    const auto bb_upper_bound_param = mMapper.PointFromGlobalToParam(bounding_box.second);
 
     // Construct element and check status
-    std::shared_ptr<Element> new_element = std::make_shared<Element>(i+1, el_lower_bound_param, el_upper_bound_param, mParameters);
+    Shared<Element> new_element = MakeShared<Element>(index+1, bb_lower_bound_param, bb_upper_bound_param, mParameters);
 
     // Check intersection status
     IntersectionStatus status{};
     if( mParameters.Get<bool>("embedding_flag") ){
-      Timer timer_check_intersect{};
-      status = static_cast<IntersectionStatus>(mpBRepOperator->GetIntersectionState(*new_element));
-      et_check_intersect += timer_check_intersect.Measure();
+      status = (*p_classifications)[index];
     }
     else { // If flag is false, consider all knotspans/ elements as inside
       status = IntersectionStatus::Inside;
@@ -73,7 +62,7 @@ void TIBRA::Run(){
     if( status == IntersectionStatus::Trimmed) {
       new_element->SetIsTrimmed(true);
       Timer timer_compute_intersection{};
-      auto p_trimmed_domain = mpBRepOperator->pGetTrimmedDomain(el_lower_bound, el_upper_bound);
+      auto p_trimmed_domain = mpBRepOperator->pGetTrimmedDomain(bounding_box.first, bounding_box.second);
       if( p_trimmed_domain ){
         new_element->pSetTrimmedDomain(p_trimmed_domain);
         valid_element = true;
