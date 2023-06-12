c268c9dac3f53f0c5b672b7185101ca07214de9a
ge69vud <manuel.messmer@tum.de>
2023-06-12 16:51:32 +0200

    Update example.

--- examples/cantilever/TIBRA_main.py
+++ examples/cantilever/TIBRA_main.py
@@ -1,25 +1,13 @@
 # Project imports
 from TIBRA_PythonApplication.PyTIBRA import PyTIBRA
 
-def neumann_condition(x, y, z):
-    return (z > (10 - 1e-6) )
-
-def dirichlet_condition(x, y, z):
-    return (z < (0.0 + 1e-6))
-
 def main():
     pytibra = PyTIBRA("TIBRAParameters.json")
     pytibra.Run()
 
     # Direct Analysis with kratos
-    surface_force = [0.0, 0.1, 0]
-    neumann_boundaries = [[neumann_condition, surface_force]]
-    penalty_factor = 1e10
-    dirichlet_boundaries = [[dirichlet_condition, penalty_factor]]
-    pytibra.RunKratosAnalysis(dirichlet_boundaries, neumann_boundaries)
+    pytibra.RunKratosAnalysis()
     pytibra.PostProcess()
-
-
 
 if __name__ == "__main__":
     main()
