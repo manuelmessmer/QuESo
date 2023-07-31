1825140778f843bc31c21af8cb21d385e9ff355b
ge69vud <manuel.messmer@tum.de>
2023-07-31 18:08:37 +0200

    Rename folder.

--- tibra/tests/steering_knuckle_kratos/test_strain_energy_steering_knuckle.py
+++ tibra/tests/steering_knuckle_kratos/test_strain_energy_steering_knuckle.py
@@ -4,21 +4,33 @@ import KratosMultiphysics
 import unittest
 
 class TestStrainEnergySteeringKnuckleKratos(unittest.TestCase):
-    def test_1(self):
-        pytibra = PyTIBRA("tibra/tests/steering_knuckle/TIBRAParameters.json")
+    def run_test(self, filename, tolerance):
+        pytibra = PyTIBRA(filename)
         pytibra.Run()
-
-        pytibra.RunKratosAnalysis("tibra/tests/steering_knuckle/KratosParameters.json")
+        pytibra.RunKratosAnalysis("tibra/tests/steering_knuckle_kratos/KratosParameters.json")
 
         analysis = pytibra.GetAnalysis()
         model_part = analysis.GetModelPart()
         strain_energy = 0.0
+
         for element in model_part.Elements:
             values = element.CalculateOnIntegrationPoints(KratosMultiphysics.STRAIN_ENERGY, model_part.ProcessInfo)
             weights = element.CalculateOnIntegrationPoints(KratosMultiphysics.INTEGRATION_WEIGHT, model_part.ProcessInfo)
             for value, weight in zip(values, weights):
                 strain_energy += value*weight
-        self.assertAlmostEqual(strain_energy, 22.67955818930111, 10)
+        self.assertAlmostEqual(strain_energy, 21.788415916271443, tolerance)
+
+    def test_1(self):
+        self.run_test("tibra/tests/steering_knuckle_kratos/TIBRAParameters1.json", 10)
+
+    def test_2(self):
+        self.run_test("tibra/tests/steering_knuckle_kratos/TIBRAParameters2.json", 10)
+
+    def test_3(self):
+        self.run_test("tibra/tests/steering_knuckle_kratos/TIBRAParameters3.json", 2)
+
+    def test_4(self):
+        self.run_test("tibra/tests/steering_knuckle_kratos/TIBRAParameters4.json", 3)
 
 if __name__ == "__main__":
     unittest.main()
