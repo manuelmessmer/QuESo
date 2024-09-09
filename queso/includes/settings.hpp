777eb72b56212b672745e30679f2ac9bdbea34ce
ge69vud <manuel.messmer@tum.de>
2024-09-09 14:20:04 +0200

    Add contitions to setting.

--- queso/includes/settings.hpp
+++ queso/includes/settings.hpp
@@ -21,18 +21,21 @@
 
 namespace queso {
 
-/// Definition of dictionart keys
+/// Definition of dictionary keys
 enum class Root {main};
-enum class Main {general_settings, mesh_settings, trimmed_quadrature_rule_settings, non_trimmed_quadrature_rule_settings};
+enum class Main {general_settings, mesh_settings, trimmed_quadrature_rule_settings, non_trimmed_quadrature_rule_settings, conditions_settings};
 enum class GeneralSettings {input_filename, output_directory_name, echo_level, embedding_flag};
 enum class MeshSettings {lower_bound_xyz, upper_bound_xyz, lower_bound_uvw, upper_bound_uvw, polynomial_order, number_of_elements};
 enum class TrimmedQuadratureRuleSettings {moment_fitting_residual, min_element_volume_ratio, min_num_boundary_triangles, use_customized_trimmed_points };
 enum class NonTrimmedQuadratureRuleSettings {integration_method};
+enum class ConditionSettings {condition_id, condition_type, input_filename, modulus, direction, value, penalty_factor};
 
-class Settings : public Dictionary<Root, Main, GeneralSettings, MeshSettings, TrimmedQuadratureRuleSettings, NonTrimmedQuadratureRuleSettings>
+typedef Dictionary<Root, Main, GeneralSettings, MeshSettings, TrimmedQuadratureRuleSettings, NonTrimmedQuadratureRuleSettings, ConditionSettings, IndexType> SettingsBaseType;
+
+class Settings : public SettingsBaseType
 {
 public:
-    typedef Dictionary<Root, Main, GeneralSettings, MeshSettings, TrimmedQuadratureRuleSettings, NonTrimmedQuadratureRuleSettings> BaseType;
+    typedef SettingsBaseType BaseType;
     typedef std::string Str;
     using BaseType::GetValue;
     using BaseType::SetValue;
@@ -40,9 +43,9 @@ public:
     bool Set = true;
     bool DontSet = false;
 
-    Settings() : BaseType(Root::main, Str("Settings")) {
+    Settings() : BaseType(Root::main, Str("settings")) {
         /// GeneralSettings
-        auto& r_general_settings = AddEmptyDictionary(Main::general_settings, Str("GeneralSettings"));
+        auto& r_general_settings = AddEmptyDictionary(Main::general_settings, Str("general_settings"));
         r_general_settings.AddValues(std::make_tuple(
             std::make_tuple(GeneralSettings::input_filename, Str("input_filename"), Str("dummy"), DontSet ),
             std::make_tuple(GeneralSettings::output_directory_name, Str("output_directory_name"), Str("queso_output"), Set ),
@@ -72,15 +75,39 @@ public:
 
         /// NonTrimmedQuadratureRuleSettings
         auto& non_trimmed_quadrature_rule_settings = AddEmptyDictionary(Main::non_trimmed_quadrature_rule_settings, Str("non_trimmed_quadrature_rule_settings"));
-         non_trimmed_quadrature_rule_settings.AddValues(std::make_tuple(
+        non_trimmed_quadrature_rule_settings.AddValues(std::make_tuple(
             std::make_tuple(NonTrimmedQuadratureRuleSettings::integration_method, Str("integration_method"), IntegrationMethod::Gauss, Set )
         ));
+
+        /// ConditionSettings
+        AddEmptyDictionary(Main::conditions_settings, Str("conditions_settings"));
+    }
+
+    SettingsBaseType& CreateNewConditionSettings() {
+        auto& r_conditions_settings = (*this)[Main::conditions_settings];
+        IndexType size = r_conditions_settings.NumberOfSubDictionaries();
+        std::string condition_name = "condition_" + std::to_string(size);
+        auto& r_new_condition_settings = r_conditions_settings.AddEmptyDictionary(size, condition_name);
+        r_new_condition_settings.AddValues(std::make_tuple(
+            std::make_tuple(ConditionSettings::condition_id, Str("condition_id"), IndexType(0), DontSet ),
+            std::make_tuple(ConditionSettings::condition_type, Str("condition_type"), std::string("dummy"), DontSet  ),
+            std::make_tuple(ConditionSettings::input_filename, Str("input_filename"), std::string("dummy"), DontSet  ),
+            std::make_tuple(ConditionSettings::modulus, Str("modulus"), 0.0, DontSet  ),
+            std::make_tuple(ConditionSettings::direction, Str("direction"), PointType{0.0, 0.0, 0.0}, DontSet  ),
+            std::make_tuple(ConditionSettings::value, Str("value"), 0.0, DontSet  ),
+            std::make_tuple(ConditionSettings::penalty_factor, Str("penalty_factor"), 0.0, DontSet  )
+        ));
+
+        return r_new_condition_settings;
     }
 
 private:
     using BaseType::AddValues;
     using BaseType::AddEmptyDictionary;
 };
+
+
+
 
 ///@}
 } // End queso namespace.
