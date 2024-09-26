85bf1daa419f732cddaba25fabec1079aabb6437
ge69vud <manuel.messmer@tum.de>
2024-09-26 14:56:17 +0200

    Add utilities to python.

---
+++ queso/python/add_utilities_to_python.cpp
@@ -0,0 +1,37 @@
+//   ____        ______  _____
+//  / __ \      |  ____|/ ____|
+// | |  | |_   _| |__  | (___   ___
+// | |  | | | | |  __|  \___ \ / _ \'
+// | |__| | |_| | |____ ____) | (_) |
+//  \___\_\\__,_|______|_____/ \___/
+//         Quadrature for Embedded Solids
+//
+//  License:    BSD 4-Clause License
+//              See: https://github.com/manuelmessmer/QuESo/blob/main/LICENSE
+//
+//  Authors:    Manuel Messmer
+
+/// Project inlcudes
+#include "queso/python/define_python.hpp"
+#include "queso/python/add_utilities_to_python.h"
+// To export
+#include "queso/utilities/mesh_utilities.h"
+
+namespace queso {
+namespace Python {
+
+namespace py = pybind11;
+
+void AddUtilitiesToPython(pybind11::module& m) {
+
+    /// Export MeshUtilites
+    py::class_<MeshUtilities>(m,"MeshUtilities")
+        .def_static("Volume", &MeshUtilities::VolumeOMP)
+        .def_static("Area", &MeshUtilities::AreaOMP)
+    ;
+
+} // End AddUtilitiesToPython
+
+} // End namespace Python
+} // End namespace queso
+
---
+++ queso/python/add_utilities_to_python.h
@@ -0,0 +1,28 @@
+//   ____        ______  _____
+//  / __ \      |  ____|/ ____|
+// | |  | |_   _| |__  | (___   ___
+// | |  | | | | |  __|  \___ \ / _ \'
+// | |__| | |_| | |____ ____) | (_) |
+//  \___\_\\__,_|______|_____/ \___/
+//         Quadrature for Embedded Solids
+//
+//  License:    BSD 4-Clause License
+//              See: https://github.com/manuelmessmer/QuESo/blob/main/LICENSE
+//
+//  Authors:    Manuel Messmer
+
+#ifndef ADD_UTILITIES_TO_PYTHON_INCLUDE_H
+#define ADD_UTILITIES_TO_PYTHON_INCLUDE_H
+
+// External includes
+#include <pybind11/pybind11.h>
+
+namespace queso {
+namespace Python {
+
+    void AddUtilitiesToPython(pybind11::module& m);
+
+} // End namespace Python
+} // End namespace queso
+
+#endif // ADD_UTILITIES_TO_PYTHON_INCLUDE_H
