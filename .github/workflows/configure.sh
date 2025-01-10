e00943c02783356b182914861c4fa19918823029
manuelmessmer <manuel.messmer@tum.de>
2025-01-10 09:23:32 +0000

    Test.

--- .github/workflows/configure.sh
+++ .github/workflows/configure.sh
@@ -8,7 +8,7 @@ export APP_SOURCE="${APP_SOURCE:-"$( cd "$(dirname "$0")" ; pwd -P )"}"
 export APP_BUILD="${APP_SOURCE}/../build/"
 
 # Set basic configuration
-export PYTHON_EXECUTABLE="/usr/bin/python3.10"
+export PYTHON_EXECUTABLE="/usr/bin/python3"
 
 echo "QuESo build type is ${QUESO_BUILD_TYPE}"
 
