ff87e2a9f364ddeaa62cdfcfa254e2595fcded57
manuelmessmer <manuel.messmer@tum.de>
2025-01-10 07:32:55 +0000

    Add test for single Set 1 and 2.

--- queso/tests/cpp_tests/test_keys.cpp
+++ queso/tests/cpp_tests/test_keys.cpp
@@ -25,7 +25,7 @@ namespace Testing {
 
 BOOST_AUTO_TEST_SUITE( KeysTestSuite )
 
-// Due to th Testing namespace, we have to establish the following typedefs.
+// Due to the Testing namespace, we have to establish the following typedefs.
 namespace key {
 typedef queso::key::SubDict SubDict;
 typedef queso::key::List List;
@@ -33,35 +33,36 @@ typedef queso::key::DataSet DataSet;
 typedef queso::key::KeyInformation KeyInformation;
 }
 
-QuESo_REGISTER_KEYS_1( TestKey1, SubDict, QuESo_LIST(zero, one, three=3, four, six=6) );
-QuESo_REGISTER_KEYS_1( TestKey2, List, QuESo_LIST(zero, four=4, five, six, seven) );
-QuESo_REGISTER_KEYS_1( TestKey3, DataSet, QuESo_LIST(zero, one) );
+QuESo_REGISTER_KEY_SET_1( TestKeys1, SubDict, QuESo_LIST(zero, one, three=3, four, six=6) );
+QuESo_REGISTER_KEY_SET_1( TestKeys2, List, QuESo_LIST(zero, four=4, five, six, seven) );
+QuESo_REGISTER_KEY_SET_1( TestKeys3, DataSet, QuESo_LIST(zero, one) );
 
 BOOST_AUTO_TEST_CASE(TestRegisterKeys1) {
     QuESo_INFO << "Testing :: Test Keys :: Register Single Key Set 1" << std::endl;
 
-    /// TestKey1
-    QuESo_CHECK_EQUAL(TestKey1::zero, 0);
-    QuESo_CHECK_EQUAL(TestKey1::one, 1);    
-    QuESo_CHECK_EQUAL(TestKey1::three, 3);  
-    QuESo_CHECK_EQUAL(TestKey1::four, 4);
-    QuESo_CHECK_EQUAL(TestKey1::six, 6);    
+    /// TestKeys1
+    QuESo_CHECK_EQUAL(TestKeys1::zero, 0);
+    QuESo_CHECK_EQUAL(TestKeys1::one, 1);    
+    QuESo_CHECK_EQUAL(TestKeys1::three, 3);  
+    QuESo_CHECK_EQUAL(TestKeys1::four, 4);
+    QuESo_CHECK_EQUAL(TestKeys1::six, 6);    
 
-    QuESo_CHECK_EQUAL( key::KeyToString(TestKey1::zero), "zero");
-    QuESo_CHECK_EQUAL( key::KeyToString(TestKey1::one), "one");
-    QuESo_CHECK_EQUAL( key::KeyToString(TestKey1::three), "three");
-    QuESo_CHECK_EQUAL( key::KeyToString(TestKey1::four), "four");
-    QuESo_CHECK_EQUAL( key::KeyToString(TestKey1::six), "six");
+    QuESo_CHECK_EQUAL( key::KeyToString(TestKeys1::zero), "zero");
+    QuESo_CHECK_EQUAL( key::KeyToString(TestKeys1::one), "one");
+    QuESo_CHECK_EQUAL( key::KeyToString(TestKeys1::three), "three");
+    QuESo_CHECK_EQUAL( key::KeyToString(TestKeys1::four), "four");
+    QuESo_CHECK_EQUAL( key::KeyToString(TestKeys1::six), "six");
     
-    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKey1SubDict>("zero"), TestKey1::zero);
-    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKey1SubDict>("one"), TestKey1::one);
-    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKey1SubDict>("three"), TestKey1::three);
-    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKey1SubDict>("four"), TestKey1::four);
-    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKey1SubDict>("six"), TestKey1::six);
+    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKeys1SubDict>("zero"), TestKeys1::zero);
+    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKeys1SubDict>("one"), TestKeys1::one);
+    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKeys1SubDict>("three"), TestKeys1::three);
+    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKeys1SubDict>("four"), TestKeys1::four);
+    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKeys1SubDict>("six"), TestKeys1::six);
+    BOOST_REQUIRE_THROW( key::StringToKey<key::TestKeys1SubDict>("two"), std::exception);
 
-    typedef decltype(key::GetKeyBaseType<TestKey1::KeyToSubDict>()) BaseType1;
-    static_assert( std::is_same<typename BaseType1::KeyToWhat, key::SubDict>::value );
-    Unique<key::KeyInformation> p_key_info_1 = MakeUnique<typename BaseType1::KeyInfo>();
+    typedef decltype(key::GetKeyBaseType<TestKeys1::KeyToSubDict>()) BaseType;
+    static_assert( std::is_same<typename BaseType::KeyToWhat, key::SubDict>::value );
+    Unique<key::KeyInformation> p_key_info_1 = MakeUnique<typename BaseType::KeyInfo>();
     
     QuESo_CHECK_EQUAL( p_key_info_1->GetKeyName(0), "zero");
     QuESo_CHECK_EQUAL( p_key_info_1->GetKeyName(1), "one");
@@ -79,78 +80,118 @@ BOOST_AUTO_TEST_CASE(TestRegisterKeys1) {
 
     QuESo_CHECK_EQUAL( p_key_info_1->GetNumberOfKeys(), 5);
 
-    QuESo_CHECK( key::IsCorrectType(p_key_info_1, TestKey1::zero) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_1, TestKey1::one) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_1, TestKey1::three) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_1, TestKey1::four) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_1, TestKey1::six) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_1, key::StringToKey<key::TestKey1SubDict>("zero")) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_1, key::StringToKey<key::TestKey1SubDict>("one")) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_1, key::StringToKey<key::TestKey1SubDict>("three")) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_1, key::StringToKey<key::TestKey1SubDict>("four")) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_1, key::StringToKey<key::TestKey1SubDict>("six")) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_1, TestKeys1::zero) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_1, TestKeys1::one) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_1, TestKeys1::three) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_1, TestKeys1::four) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_1, TestKeys1::six) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_1, key::StringToKey<key::TestKeys1SubDict>("zero")) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_1, key::StringToKey<key::TestKeys1SubDict>("one")) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_1, key::StringToKey<key::TestKeys1SubDict>("three")) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_1, key::StringToKey<key::TestKeys1SubDict>("four")) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_1, key::StringToKey<key::TestKeys1SubDict>("six")) );
 
-    QuESo_CHECK( !key::IsCorrectType(p_key_info_1, TestKey2::zero ));
+    QuESo_CHECK( !key::IsCorrectType(p_key_info_1, TestKeys2::zero ));
 
-    QuESo_CHECK_EQUAL( p_key_info_1->GetAllKeyNames(), "[zero, one, three, four, six]" )
+    QuESo_CHECK_EQUAL( p_key_info_1->GetAllKeyNames(), "['zero', 'one', 'three', 'four', 'six']" )
 }
 
 BOOST_AUTO_TEST_CASE(TestRegisterKeys2) {
     QuESo_INFO << "Testing :: Test Keys :: Register Single Key Set 2" << std::endl;
 
-    /// TestKey2
-    QuESo_CHECK_EQUAL(TestKey2::zero, 0);
-    QuESo_CHECK_EQUAL(TestKey2::four, 4);
-    QuESo_CHECK_EQUAL(TestKey2::five, 5);
-    QuESo_CHECK_EQUAL(TestKey2::six, 6);
-    QuESo_CHECK_EQUAL(TestKey2::seven, 7);
+    /// TestKeys2
+    QuESo_CHECK_EQUAL(TestKeys2::zero, 0);
+    QuESo_CHECK_EQUAL(TestKeys2::four, 4);    
+    QuESo_CHECK_EQUAL(TestKeys2::five, 5);  
+    QuESo_CHECK_EQUAL(TestKeys2::six, 6);
+    QuESo_CHECK_EQUAL(TestKeys2::seven, 7);    
 
-    QuESo_CHECK_EQUAL( key::KeyToString(TestKey2::zero), "zero");
-    QuESo_CHECK_EQUAL( key::KeyToString(TestKey2::four), "four");
-    QuESo_CHECK_EQUAL( key::KeyToString(TestKey2::five), "five");
-    QuESo_CHECK_EQUAL( key::KeyToString(TestKey2::six), "six");
-    QuESo_CHECK_EQUAL( key::KeyToString(TestKey2::seven), "seven");
+    QuESo_CHECK_EQUAL( key::KeyToString(TestKeys2::zero), "zero");
+    QuESo_CHECK_EQUAL( key::KeyToString(TestKeys2::four), "four");
+    QuESo_CHECK_EQUAL( key::KeyToString(TestKeys2::five), "five");
+    QuESo_CHECK_EQUAL( key::KeyToString(TestKeys2::six), "six");
+    QuESo_CHECK_EQUAL( key::KeyToString(TestKeys2::seven), "seven");
+    
+    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKeys2List>("zero"), TestKeys2::zero);
+    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKeys2List>("four"), TestKeys2::four);
+    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKeys2List>("five"), TestKeys2::five);
+    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKeys2List>("six"), TestKeys2::six);
+    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKeys2List>("seven"), TestKeys2::seven);
+    BOOST_REQUIRE_THROW( key::StringToKey<key::TestKeys2List>("three"), std::exception);
 
-    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKey2List>("zero"), TestKey2::zero);
-    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKey2List>("four"), TestKey2::four);
-    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKey2List>("five"), TestKey2::five);
-    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKey2List>("six"), TestKey2::six);
-    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKey2List>("seven"), TestKey2::seven);
-
-    typedef decltype(key::GetKeyBaseType<TestKey2::KeyToList>()) BaseType2;
-    static_assert( std::is_same<typename BaseType2::KeyToWhat, key::List>::value );
-    Unique<key::KeyInformation> p_key_info_2 = MakeUnique<typename BaseType2::KeyInfo>();
-
+    typedef decltype(key::GetKeyBaseType<TestKeys2::KeyToList>()) BaseType;
+    static_assert( std::is_same<typename BaseType::KeyToWhat, key::List>::value );
+    Unique<key::KeyInformation> p_key_info_2 = MakeUnique<typename BaseType::KeyInfo>();
+    
     QuESo_CHECK_EQUAL( p_key_info_2->GetKeyName(0), "zero");
     QuESo_CHECK_EQUAL( p_key_info_2->GetKeyName(4), "four");
     QuESo_CHECK_EQUAL( p_key_info_2->GetKeyName(5), "five");
     QuESo_CHECK_EQUAL( p_key_info_2->GetKeyName(6), "six");
     QuESo_CHECK_EQUAL( p_key_info_2->GetKeyName(7), "seven");
-    BOOST_REQUIRE_THROW( p_key_info_2->GetKeyName(1), std::exception );
+    BOOST_REQUIRE_THROW( p_key_info_2->GetKeyName(3), std::exception );
 
     QuESo_CHECK_EQUAL( p_key_info_2->GetKeyValue("zero"), 0);
     QuESo_CHECK_EQUAL( p_key_info_2->GetKeyValue("four"), 4);
     QuESo_CHECK_EQUAL( p_key_info_2->GetKeyValue("five"), 5);
     QuESo_CHECK_EQUAL( p_key_info_2->GetKeyValue("six"), 6);
     QuESo_CHECK_EQUAL( p_key_info_2->GetKeyValue("seven"), 7);
-    BOOST_REQUIRE_THROW( p_key_info_2->GetKeyValue("one"), std::exception );
+    BOOST_REQUIRE_THROW( p_key_info_2->GetKeyValue("three"), std::exception );    
 
     QuESo_CHECK_EQUAL( p_key_info_2->GetNumberOfKeys(), 5);
 
-    QuESo_CHECK( key::IsCorrectType(p_key_info_2, TestKey2::zero) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_2, TestKey2::four) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_2, TestKey2::five) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_2, TestKey2::six) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_2, TestKey2::seven) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_2, key::StringToKey<key::TestKey2List>("zero")) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_2, key::StringToKey<key::TestKey2List>("four")) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_2, key::StringToKey<key::TestKey2List>("five")) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_2, key::StringToKey<key::TestKey2List>("six")) );
-    QuESo_CHECK( key::IsCorrectType(p_key_info_2, key::StringToKey<key::TestKey2List>("seven")) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_2, TestKeys2::zero) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_2, TestKeys2::four) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_2, TestKeys2::five) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_2, TestKeys2::six) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_2, TestKeys2::seven) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_2, key::StringToKey<key::TestKeys2List>("zero")) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_2, key::StringToKey<key::TestKeys2List>("four")) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_2, key::StringToKey<key::TestKeys2List>("five")) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_2, key::StringToKey<key::TestKeys2List>("six")) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_2, key::StringToKey<key::TestKeys2List>("seven")) );
 
-    QuESo_CHECK( !key::IsCorrectType(p_key_info_2, TestKey1::zero ));
+    QuESo_CHECK( !key::IsCorrectType(p_key_info_2, TestKeys1::zero ));
 
-    QuESo_CHECK_EQUAL( p_key_info_2->GetAllKeyNames(), "[zero, four, five, six, seven]" )
+    QuESo_CHECK_EQUAL( p_key_info_2->GetAllKeyNames(), "['zero', 'four', 'five', 'six', 'seven']" )
+}
+
+BOOST_AUTO_TEST_CASE(TestRegisterKeys3) {
+    QuESo_INFO << "Testing :: Test Keys :: Register Single Key Set 3" << std::endl;
+
+    /// TestKeys3
+    QuESo_CHECK_EQUAL(TestKeys3::zero, 0);
+    QuESo_CHECK_EQUAL(TestKeys3::one, 1);
+
+    QuESo_CHECK_EQUAL( key::KeyToString(TestKeys3::zero), "zero");
+    QuESo_CHECK_EQUAL( key::KeyToString(TestKeys3::one), "one");
+
+    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKeys3DataSet>("zero"), TestKeys3::zero);
+    QuESo_CHECK_EQUAL( key::StringToKey<key::TestKeys3DataSet>("one"), TestKeys3::one);
+    BOOST_REQUIRE_THROW( key::StringToKey<key::TestKeys3DataSet>("two"), std::exception);
+
+    typedef decltype(key::GetKeyBaseType<TestKeys3::KeyToDataSet>()) BaseType;
+    static_assert( std::is_same<typename BaseType::KeyToWhat, key::DataSet>::value );
+    Unique<key::KeyInformation> p_key_info_3 = MakeUnique<typename BaseType::KeyInfo>();
+
+    QuESo_CHECK_EQUAL( p_key_info_3->GetKeyName(0), "zero");
+    QuESo_CHECK_EQUAL( p_key_info_3->GetKeyName(1), "one");
+    BOOST_REQUIRE_THROW( p_key_info_3->GetKeyName(2), std::exception );
+
+    QuESo_CHECK_EQUAL( p_key_info_3->GetKeyValue("zero"), 0);
+    QuESo_CHECK_EQUAL( p_key_info_3->GetKeyValue("one"), 1);
+    BOOST_REQUIRE_THROW( p_key_info_3->GetKeyValue("two"), std::exception );
+
+    QuESo_CHECK_EQUAL( p_key_info_3->GetNumberOfKeys(), 2);
+
+    QuESo_CHECK( key::IsCorrectType(p_key_info_3, TestKeys3::zero) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_3, TestKeys3::one) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_3, key::StringToKey<key::TestKeys3DataSet>("zero")) );
+    QuESo_CHECK( key::IsCorrectType(p_key_info_3, key::StringToKey<key::TestKeys3DataSet>("one")) );
+
+    QuESo_CHECK( !key::IsCorrectType(p_key_info_3, TestKeys1::zero) );
+    QuESo_CHECK( !key::IsCorrectType(p_key_info_3, TestKeys2::zero) );
+
+    QuESo_CHECK_EQUAL( p_key_info_3->GetAllKeyNames(), "['zero', 'one']" )
 }
 
 BOOST_AUTO_TEST_SUITE_END()
