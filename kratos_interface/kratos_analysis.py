40f9c0795091816dc3ce721109e9a158adbf7425
manuel.messmer <manuel.messmer@tum.de>
2025-04-17 15:53:23 +0200

    Improve doxygen.

--- kratos_interface/kratos_analysis.py
+++ kratos_interface/kratos_analysis.py
@@ -1,29 +1,43 @@
+import os
 # Import Kratos
 import KratosMultiphysics as KM
-
-import KratosMultiphysics.StructuralMechanicsApplication as StructuralMechanicsApplication
-import KratosMultiphysics.IgaApplication as IgaApplication
-import KratosMultiphysics.python_linear_solver_factory as python_linear_solver_factory
-import KratosMultiphysics.LinearSolversApplication as LinearSolversApplication
-
 # Project imports
 from kratos_interface.custom_analysis_stage import CustomAnalysisStage
-#External imports
-import os
 
 def GetFilePath(fileName):
     return os.path.dirname(__file__) + "/" + fileName
 
 class Analysis():
-    """Wrapper for customized Kratos analysis stage.
+    """
+    Wrapper for customized Kratos analysis stage.
 
-    Runs simulation.
+    This class provides a wrapper around the `CustomAnalysisStage` to run the simulation in Kratos.
+    It sets up the model, creates the elements, applies the boundary conditions, and executes the analysis.
     """
     def __init__(self, settings, kratos_settings_filename, elements, boundary_conditions ):
-        """The constructor."""
+        """
+        Constructor for the `Analysis` class.
+
+        This constructor initializes the Kratos model and sets up the `CustomAnalysisStage` with the given
+        settings, elements, and boundary conditions. It then runs the analysis.
+
+        Args:
+            settings (Parameters): The settings for the analysis, including elements and boundary conditions.
+            kratos_settings_filename (str): The path to the Kratos settings file in JSON format.
+            elements (list): A list of elements (from QuESo) to be added to the model part.
+            boundary_conditions (list): A list of boundary conditions (from QuESo) to be applied to the model.
+        """
         self.model = KM.Model()
         analysis = CustomAnalysisStage(self.model, settings, kratos_settings_filename, elements, boundary_conditions)
         analysis.Run()
 
     def GetModelPart(self):
+        """
+        Returns the Kratos model part created in the analysis.
+
+        This method retrieves the model part named "NurbsMesh" from the Kratos model.
+
+        Returns:
+            ModelPart: The model part named "NurbsMesh" in the Kratos model.
+        """
         return self.model.GetModelPart("NurbsMesh")
