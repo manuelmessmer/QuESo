d579191e6f0053964724f7ade678110db9d33a18
manuel.messmer <manuel.messmer@tum.de>
2025-04-17 17:51:27 +0200

    Refactor analysis.

--- kratos_interface/kratos_analysis.py
+++ kratos_interface/kratos_analysis.py
@@ -1,8 +1,10 @@
 import os
-# Import Kratos
-import KratosMultiphysics as KM
+# Import QuESo
+import QuESo_PythonApplication as QuESo
 # Project imports
 from kratos_interface.custom_analysis_stage import CustomAnalysisStage
+# Import Kratos
+import KratosMultiphysics as KM
 
 def GetFilePath(fileName):
     return os.path.dirname(__file__) + "/" + fileName
@@ -14,7 +16,12 @@ class Analysis():
     This class provides a wrapper around the `CustomAnalysisStage` to run the simulation in Kratos.
     It sets up the model, creates the elements, applies the boundary conditions, and executes the analysis.
     """
-    def __init__(self, settings, kratos_settings_filename, elements, boundary_conditions ):
+    def __init__(self,
+            settings: QuESo.Settings,
+            kratos_settings_filename: str,
+            elements: QuESo.ElementVector,
+            boundary_conditions: QuESo.ConditionVector
+        ) -> None:
         """
         Constructor for the `Analysis` class.
 
@@ -22,16 +29,16 @@ class Analysis():
         settings, elements, and boundary conditions. It then runs the analysis.
 
         Args:
-            settings (Parameters): The settings for the analysis, including elements and boundary conditions.
+            settings (QuESo.Settings): The settings for the analysis, including elements and boundary conditions.
             kratos_settings_filename (str): The path to the Kratos settings file in JSON format.
-            elements (list): A list of elements (from QuESo) to be added to the model part.
-            boundary_conditions (list): A list of boundary conditions (from QuESo) to be applied to the model.
+            elements (QuESo.ElementVector): A list of elements to be added to the model part.
+            boundary_conditions (QuESo.ConditionVector): A list of boundary conditions to be applied to the model.
         """
         self.model = KM.Model()
         analysis = CustomAnalysisStage(self.model, settings, kratos_settings_filename, elements, boundary_conditions)
         analysis.Run()
 
-    def GetModelPart(self):
+    def GetModelPart(self) -> None:
         """
         Returns the Kratos model part created in the analysis.
 
