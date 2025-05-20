113ec098db4783908a84c49817c03cf7a8ba7b2e
manuel.messmer <manuel.messmer@tum.de>
2025-05-20 13:15:35 +0200

    Add string access tests.

--- queso/tests/cpp_tests/test_dictionary.cpp
+++ queso/tests/cpp_tests/test_dictionary.cpp
@@ -22,7 +22,6 @@
 
 namespace queso {
 
-
 namespace Testing {
 
 BOOST_AUTO_TEST_SUITE( DictionaryTestSuite )
@@ -155,6 +154,153 @@ BOOST_AUTO_TEST_CASE(DictionaryKeyAccessTypeTest) {
         QuESo_CHECK_NEAR( items->GetValueFast<double>(TestKeys3::three), 3.0, 1e-10);
         QuESo_CHECK_EQUAL( items->GetValueFast<IndexType>(TestKeys3::four), 3u);
         QuESo_CHECK_EQUAL( items->GetValueFast<std::string>(TestKeys3::five), "Hello");
+    }
+}
+
+
+BOOST_AUTO_TEST_CASE(DictionaryStringAccessTypeTest) {
+    QuESo_INFO << "Testing :: Test Dictionary :: String access" << std::endl;
+
+    // Create dictionary
+    using DictionaryType = Dictionary<queso::key::MainValuesTypeTag>;
+    DictionaryType test_dict( DictionaryType::KeySetInfoTypeTag<
+        key::detail::TestKeys5MainValuesTypeTagKeySetInfo,
+        key::detail::TestKeys5SubDictTypeTagKeySetInfo,
+        key::detail::TestKeys5ListTypeTagKeySetInfo>{} );
+
+    using StringAccess = DictionaryStringAccess<DictionaryType>;
+
+    // Add some values
+    QuESo_CHECK( !StringAccess::IsSet(test_dict, "seven") );
+    QuESo_CHECK( !StringAccess::IsSet(test_dict, "eight") );
+    QuESo_CHECK( !StringAccess::IsSet(test_dict, "nine") );
+    QuESo_CHECK( !StringAccess::IsSet(test_dict, "ten") );
+
+    StringAccess::SetValue(test_dict, "seven", 2.0);
+    StringAccess::SetValue(test_dict, "eight", 3u );
+
+    /*---- Fill dictionary ----*/
+    {
+        // Add SubDictionary 1.
+        auto p_subdict_1 = MakeUnique<DictionaryType>( DictionaryType::KeySetInfoTypeTag<
+            std::false_type,
+            key::detail::TestKeys1SubDictTypeTagKeySetInfo,
+            std::false_type>{} );
+
+        if constexpr (!NOTDEBUG) {
+            // Dictionary has an empty data set.
+            BOOST_REQUIRE_THROW( StringAccess::IsSet(*p_subdict_1, "zero"), std::exception );
+            BOOST_REQUIRE_THROW( StringAccess::SetValue(*p_subdict_1, "zero", PointType({2.0, 3.2, 1.1})), std::exception );
+        }
+
+        { // Add SubDictionary 2.
+            auto p_subdict_2 = MakeUnique<DictionaryType>( DictionaryType::KeySetInfoTypeTag<
+                key::detail::TestKeys3MainValuesTypeTagKeySetInfo,
+                std::false_type,
+                std::false_type>{} );
+
+            if constexpr (!NOTDEBUG) {
+                // Dictionary does not contain any subdictionaries.
+                BOOST_REQUIRE_THROW( StringAccess::SetSubDictionary(*p_subdict_2, "zero", std::move(p_subdict_2)), std::exception );
+            }
+
+            StringAccess::SetValue(*p_subdict_2, "zero", PointType({2.0, 3.2, 1.1}));
+            StringAccess::SetValue(*p_subdict_2, "one", Vector3i({1, 2, 3}));
+            StringAccess::SetValue(*p_subdict_2, "two", false);
+
+            if constexpr (!NOTDEBUG) {
+                // Wrong key type.
+                BOOST_REQUIRE_THROW( StringAccess::SetSubDictionary(*p_subdict_1, "zero", std::move(p_subdict_2)), std::exception );
+            }
+            StringAccess::SetSubDictionary(*p_subdict_1, "zero", std::move(p_subdict_2));
+        }
+
+        StringAccess::SetSubDictionary(test_dict, "zero", std::move(p_subdict_1) );
+
+        // Add List  containig SubDictionary 3.
+        auto p_subdict_3 = MakeUnique<DictionaryType>( DictionaryType::KeySetInfoTypeTag<
+            key::detail::TestKeys3MainValuesTypeTagKeySetInfo,
+            std::false_type,
+            std::false_type>{} );
+
+        StringAccess::SetValue(*p_subdict_3, "three", 3.0);
+        StringAccess::SetValue(*p_subdict_3, "four", 3u);
+        StringAccess::SetValue(*p_subdict_3, "five", std::string("Hello"));
+
+        if constexpr (!NOTDEBUG) {
+            // Dictionary does not contain any list.
+            BOOST_REQUIRE_THROW( StringAccess::GetList(*p_subdict_3, "five"), std::exception );
+            // Wrong key type.
+            BOOST_REQUIRE_THROW( StringAccess::GetList(test_dict, "five"), std::exception );
+        }
+        auto& list_a = StringAccess::GetList(test_dict, "five");
+        list_a.push_back(std::move(p_subdict_3));
+
+        // Add List  containig SubDictionary 3.
+        auto p_subdict_3 = MakeUnique<DictionaryType>( DictionaryType::KeySetInfoTypeTag<
+            key::detail::TestKeys3MainValuesTypeTagKeySetInfo,
+            std::false_type,
+            std::false_type>{} );
+
+        StringAccess::SetValue(*p_subdict_3, "three", 3.0);
+        StringAccess::SetValue(*p_subdict_3, "four", 3u);
+        StringAccess::SetValue(*p_subdict_3, "five", std::string("Hello"));
+
+        if constexpr (!NOTDEBUG) {
+            // Dictionary does not contain any list.
+            BOOST_REQUIRE_THROW( p_subdict_3->GetList(TestKeys5::five), std::exception );
+            // Wrong key type.
+            BOOST_REQUIRE_THROW( test_dict.GetList(TestKeys4::five), std::exception );
+        }
+        auto& list_a = test_dict.GetList(TestKeys5::five);
+        list_a.push_back(std::move(p_subdict_3));
+    }
+
+    /*---- Check dictionary ----*/
+
+    // Values are set.
+    QuESo_CHECK( StringAccess::IsSet(test_dict, "seven") );
+    QuESo_CHECK( StringAccess::IsSet(test_dict, "eight") );
+
+    QuESo_CHECK_NEAR( StringAccess::GetValue<double>(test_dict, "seven"), 2.0, 1e-10);
+    QuESo_CHECK_EQUAL( StringAccess::GetValue<IndexType>(test_dict, "eight" ), 3u );
+
+    // Values are not set.
+    QuESo_CHECK( !StringAccess::IsSet(test_dict, "nine") );
+    QuESo_CHECK( !StringAccess::IsSet(test_dict, "ten") );
+
+    // Check Subdictionary.
+    auto& r_subdict_1 = test_dict[TestKeys5::zero];
+
+    if constexpr (!NOTDEBUG) {
+        // Dictionary has an empty data set.
+        BOOST_REQUIRE_THROW( StringAccess::IsSet(r_subdict_1, "zero"), std::exception );
+        BOOST_REQUIRE_THROW( StringAccess::GetValue<PointType>(r_subdict_1, "zero"), std::exception );
+        // Wrong key.
+        BOOST_REQUIRE_THROW( r_subdict_1[TestKeys4::zero], std::exception );
+    }
+
+    const auto& r_subdict_2 = r_subdict_1[TestKeys1::zero];
+    if constexpr (!NOTDEBUG) {
+        // Dictionary does not contain any subdictionaries.
+        BOOST_REQUIRE_THROW( r_subdict_2[TestKeys1::zero], std::exception );
+    }
+
+    QuESo_CHECK( StringAccess::IsSet(r_subdict_2, "zero") );
+    QuESo_CHECK( StringAccess::IsSet(r_subdict_2, "one") );
+    QuESo_CHECK( StringAccess::IsSet(r_subdict_2, "two") );
+
+    QuESo_CHECK_POINT_NEAR( StringAccess::GetValue<PointType>(r_subdict_2, "zero"), PointType({2.0, 3.2, 1.1}), 1e-10 );
+    QuESo_CHECK_Vector3i_EQUAL( StringAccess::GetValue<Vector3i>(r_subdict_2, "one"), Vector3i({1, 2, 3}) );
+    QuESo_CHECK( !StringAccess::GetValue<bool>(r_subdict_2, "two") );
+
+    QuESo_CHECK( !StringAccess::IsSet(r_subdict_2, "five") );
+
+    // Check List (Note: list has just one item).
+    for( const auto& items : test_dict.GetList(TestKeys5::five) ) {
+        QuESo_CHECK_NEAR( StringAccess::GetValue<double>(*items, "three"), 3.0, 1e-10);
+        QuESo_CHECK_EQUAL( StringAccess::GetValue<IndexType>(*items, "four"), 3u);
+        QuESo_CHECK_EQUAL( StringAccess::GetValue<std::string>(*items, "five"), "Hello");
     }
 }
 
