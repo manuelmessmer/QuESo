22ecba3f18574fbacf57e1ef5fb791ebda4216e2
manuel.messmer <manuel.messmer@tum.de>
2025-05-20 12:59:12 +0200

    Clean.

--- queso/tests/cpp_tests/test_dictionary.cpp
+++ queso/tests/cpp_tests/test_dictionary.cpp
@@ -27,16 +27,17 @@ namespace Testing {
 
 BOOST_AUTO_TEST_SUITE( DictionaryTestSuite )
 
-BOOST_AUTO_TEST_CASE(SettingsWrongTypeTest) {
+BOOST_AUTO_TEST_CASE(DictionaryKeyAccessTypeTest) {
     QuESo_INFO << "Testing :: Test Dictionary :: Key access" << std::endl;
 
-    using SettingsBaseType = Dictionary<queso::key::MainValuesTypeTag>;
-    SettingsBaseType test_dict( SettingsBaseType::KeySetInfoTypeTag<
+    // Create dictionary
+    using DictionaryType = Dictionary<queso::key::MainValuesTypeTag>;
+    DictionaryType test_dict( DictionaryType::KeySetInfoTypeTag<
         key::detail::TestKeys5MainValuesTypeTagKeySetInfo,
         key::detail::TestKeys5SubDictTypeTagKeySetInfo,
         key::detail::TestKeys5ListTypeTagKeySetInfo>{} );
 
-    // Values
+    // Add some values
     QuESo_CHECK( !test_dict.IsSet(TestKeys5::seven) );
     QuESo_CHECK( !test_dict.IsSet(TestKeys5::eight) );
     QuESo_CHECK( !test_dict.IsSet(TestKeys5::nine) );
@@ -45,9 +46,10 @@ BOOST_AUTO_TEST_CASE(SettingsWrongTypeTest) {
     test_dict.SetValue(TestKeys5::seven, 2.0);
     test_dict.SetValue(TestKeys5::eight, 3u );
 
-    { // Fill Dictionary
-        // SubDictionary 1
-        auto p_subdict_1 = MakeUnique<SettingsBaseType>( SettingsBaseType::KeySetInfoTypeTag<
+    /*---- Fill dictionary ----*/
+    {
+        // Add SubDictionary 1.
+        auto p_subdict_1 = MakeUnique<DictionaryType>( DictionaryType::KeySetInfoTypeTag<
             std::false_type,
             key::detail::TestKeys1SubDictTypeTagKeySetInfo,
             std::false_type>{} );
@@ -58,8 +60,8 @@ BOOST_AUTO_TEST_CASE(SettingsWrongTypeTest) {
             BOOST_REQUIRE_THROW( p_subdict_1->SetValue(TestKeys3::zero, PointType({2.0, 3.2, 1.1})), std::exception );
         }
 
-        {
-            auto p_subdict_2 = MakeUnique<SettingsBaseType>( SettingsBaseType::KeySetInfoTypeTag<
+        { // Add SubDictionary 2.
+            auto p_subdict_2 = MakeUnique<DictionaryType>( DictionaryType::KeySetInfoTypeTag<
                 key::detail::TestKeys3MainValuesTypeTagKeySetInfo,
                 std::false_type,
                 std::false_type>{} );
@@ -82,8 +84,8 @@ BOOST_AUTO_TEST_CASE(SettingsWrongTypeTest) {
 
         test_dict.SetSubDictionary(TestKeys5::zero, std::move(p_subdict_1) );
 
-        // Add a dict to a list
-        auto p_subdict_3 = MakeUnique<SettingsBaseType>( SettingsBaseType::KeySetInfoTypeTag<
+        // Add List  containig SubDictionary 3.
+        auto p_subdict_3 = MakeUnique<DictionaryType>( DictionaryType::KeySetInfoTypeTag<
             key::detail::TestKeys3MainValuesTypeTagKeySetInfo,
             std::false_type,
             std::false_type>{} );
@@ -95,16 +97,16 @@ BOOST_AUTO_TEST_CASE(SettingsWrongTypeTest) {
         if constexpr (!NOTDEBUG) {
             // Dictionary does not contain any list.
             BOOST_REQUIRE_THROW( p_subdict_3->GetList(TestKeys5::five), std::exception );
-            // Wrong key type
+            // Wrong key type.
             BOOST_REQUIRE_THROW( test_dict.GetList(TestKeys4::five), std::exception );
         }
         auto& list_a = test_dict.GetList(TestKeys5::five);
         list_a.push_back(std::move(p_subdict_3));
     }
 
-    /* Check Dictionary */
+    /*---- Check dictionary ----*/
 
-    // Values are set
+    // Values are set.
     QuESo_CHECK( test_dict.IsSet(TestKeys5::seven) );
     QuESo_CHECK( test_dict.IsSet(TestKeys5::eight) );
 
@@ -113,24 +115,22 @@ BOOST_AUTO_TEST_CASE(SettingsWrongTypeTest) {
     QuESo_CHECK_NEAR( test_dict.GetValueFast<double>(TestKeys5::seven), 2.0, 1e-10);
     QuESo_CHECK_EQUAL( test_dict.GetValueFast<IndexType>(TestKeys5::eight ), 3u );
 
-    // Values are not set
+    // Values are not set.
     QuESo_CHECK( !test_dict.IsSet(TestKeys5::nine) );
     QuESo_CHECK( !test_dict.IsSet(TestKeys5::ten) );
 
-    // Check Subdictionary
+    // Check Subdictionary.
     auto& r_subdict_1 = test_dict[TestKeys5::zero];
 
     if constexpr (!NOTDEBUG) {
         // Dictionary has an empty data set.
         BOOST_REQUIRE_THROW( r_subdict_1.IsSet(TestKeys3::zero), std::exception );
         BOOST_REQUIRE_THROW( r_subdict_1.GetValue<PointType>(TestKeys3::zero), std::exception );
-        // Wrong key
+        // Wrong key.
         BOOST_REQUIRE_THROW( r_subdict_1[TestKeys4::zero], std::exception );
     }
 
-
     const auto& r_subdict_2 = r_subdict_1[TestKeys1::zero];
-
     if constexpr (!NOTDEBUG) {
         // Dictionary does not contain any subdictionaries.
         BOOST_REQUIRE_THROW( r_subdict_2[TestKeys1::zero], std::exception );
@@ -146,7 +146,7 @@ BOOST_AUTO_TEST_CASE(SettingsWrongTypeTest) {
 
     QuESo_CHECK( !r_subdict_2.IsSet(TestKeys3::five) );
 
-    // Check List (Note: list has just one item)
+    // Check List (Note: list has just one item).
     for( const auto& items : test_dict.GetList(TestKeys5::five) ) {
         QuESo_CHECK_NEAR( items->GetValue<double>(TestKeys3::three), 3.0, 1e-10);
         QuESo_CHECK_EQUAL( items->GetValue<IndexType>(TestKeys3::four), 3u);
@@ -156,7 +156,6 @@ BOOST_AUTO_TEST_CASE(SettingsWrongTypeTest) {
         QuESo_CHECK_EQUAL( items->GetValueFast<IndexType>(TestKeys3::four), 3u);
         QuESo_CHECK_EQUAL( items->GetValueFast<std::string>(TestKeys3::five), "Hello");
     }
-
 }
 
 BOOST_AUTO_TEST_SUITE_END()
