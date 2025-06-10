51ce628eaf97f6368400baa8db2570723783e526
manuel.messmer <manuel.messmer@tum.de>
2025-06-10 17:56:36 +0200

    Use composition.

--- queso/includes/dereference_iterator.hpp
+++ queso/includes/dereference_iterator.hpp
@@ -27,9 +27,9 @@ namespace queso {
 ///        std::vector<std::unique_ptr<Type>>. It allows to iterate through the container
 ///        without the need of dereferencing the unique_ptr.
 /// @tparam BaseIterator
-template <class BaseIterator> class DereferenceIterator : public BaseIterator {
+template <class BaseIterator>
+class DereferenceIterator {
 public:
-
     static constexpr bool is_const = std::is_const<
         std::remove_reference_t<decltype(*std::declval<BaseIterator>())>
     >::value;
@@ -45,31 +45,39 @@ public:
         value_type&                             // Otherwise, use non-const reference
     >::type;
 
-    explicit constexpr DereferenceIterator(const BaseIterator &rOther) : BaseIterator(rOther) {}
+    explicit constexpr DereferenceIterator(const BaseIterator &rOther) : mIt(rOther) {}
 
     [[nodiscard]] constexpr reference operator*() const noexcept {
-        return *(BaseIterator::operator*());
+        return *(*mIt);
     }
     [[nodiscard]] constexpr pointer operator->() const noexcept {
-        return BaseIterator::operator*().get();
+        return mIt->get();
     }
-    [[nodiscard]] constexpr reference operator[](IndexType n) const noexcept {
-        return *(BaseIterator::operator[](n));
+    [[nodiscard]] constexpr reference operator[](ptrdiff_t n) const noexcept {
+        return *(*(mIt + n));
     }
 
     constexpr DereferenceIterator& operator++() noexcept {
-        BaseIterator::operator++();
+        ++mIt;
         return *this;
     }
     constexpr DereferenceIterator operator++(int) noexcept {
         DereferenceIterator tmp(*this);
-        BaseIterator::operator++();
+        ++mIt;
         return tmp;
     }
     constexpr DereferenceIterator& operator+=(ptrdiff_t inc) noexcept {
-        BaseIterator::operator+=(inc);
+        mIt += inc;
         return *this;
     }
+
+    // Provide access to the underlying iterator for operator+
+    [[nodiscard]] constexpr const BaseIterator& base() const noexcept {
+        return mIt;
+    }
+
+private:
+    BaseIterator mIt;
 };
 
 template <typename Iterator>
@@ -80,13 +88,13 @@ template <typename Iterator>
 template <class BaseIterator>
 [[nodiscard]] constexpr DereferenceIterator<BaseIterator> operator+(
         const DereferenceIterator<BaseIterator>& it, ptrdiff_t n) noexcept {
-    return DereferenceIterator<BaseIterator>(static_cast<const BaseIterator&>(it) + n);
+    return DereferenceIterator<BaseIterator>(it.base() + n);
 }
 
 template <class BaseIterator>
 [[nodiscard]] constexpr DereferenceIterator<BaseIterator> operator+(
         ptrdiff_t n, const DereferenceIterator<BaseIterator>& it) noexcept {
-    return DereferenceIterator<BaseIterator>(n + static_cast<const BaseIterator&>(it));
+    return DereferenceIterator<BaseIterator>(n + it.base());
 }
 
 /// @brief Enables to use range-based for loops with DereferenceIterator.
