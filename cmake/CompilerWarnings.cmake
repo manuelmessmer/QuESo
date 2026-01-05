31b283d4d9b8c131c7ce8349f31a13b381dc892e
manuelmessmer <manuel.messmer@tum.de>
2026-01-05 16:38:59 +0100

    Add bunch of additional gcc warnings

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -67,20 +67,20 @@ function(
             # catch hard to track down memory errors
             -Wold-style-cast # warn for c-style casts
             -Wcast-align # warn for potential performance problem casts
-            #-Wunused # warn on anything being unused
+            -Wunused # warn on anything being unused
             -Woverloaded-virtual # warn if you overload (not override) a virtual function
             -Wpedantic # warn if non-standard C++ is used
             -Wconversion # warn on type conversions that may lose data
             -Wsign-conversion # warn on sign conversions
             -Wnull-dereference # warn if a null dereference is detected
             -Wdouble-promotion # warn if float is implicit promoted to double
-            #-Wformat=2 # warn on security issues around functions that format output (ie printf)
-            #-Wimplicit-fallthrough # warn on statements that fallthrough without an explicit annotation
-            #-Wmisleading-indentation # warn if indentation implies blocks where blocks do not exist
-            #-Wduplicated-cond # warn if if / else chain has duplicated conditions
+            -Wformat=2 # warn on security issues around functions that format output (ie printf)
+            -Wimplicit-fallthrough # warn on statements that fallthrough without an explicit annotation
+            -Wmisleading-indentation # warn if indentation implies blocks where blocks do not exist
+            -Wduplicated-cond # warn if if / else chain has duplicated conditions
             -Wduplicated-branches # warn if if / else branches have duplicated code
-            #-Wlogical-op # warn about logical operations being used where bitwise were probably wanted
-            #-Wuseless-cast # warn if you perform a cast to the same type
+            -Wlogical-op # warn about logical operations being used where bitwise were probably wanted
+            -Wuseless-cast # warn if you perform a cast to the same type
             -Wsuggest-override # warn if an overridden member function is not marked 'override' or 'final'
         )
     endif()
